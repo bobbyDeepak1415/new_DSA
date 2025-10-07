b134de826186dc2d0bd44968c9f52f9eda271fdb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-08 02:52:02 +0530

    second

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
 
 // console.log(display(5));
 
+// ------------------------------------------------
 // 2.sum of digits of a number
 
 // function digits(n) {
@@ -27,12 +28,16 @@
 // }
 
 // console.log(digits(457))
-function digits(n) {
 
-  
-    
+function digits(num) {
+  let sum = 0;
 
+  while (num > 0) {
+    sum += num % 10;
+    num = Math.floor(num / 10);
+  }
 
+  return sum;
 }
 
-console.log(digits(457))
+console.log(digits(493));
