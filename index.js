82167739dcb1e18ccac01e3aed8ffea743268708
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-08 00:37:35 +0530

    first

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 // 1. sum of 1 to n
 
-function display(n) {
-  let sum = 0;
+// function display(n) {
+//   let sum = 0;
 
-  for (let i = 1; i <= n; i++) {
-    sum = sum + i;
-  }
+//   for (let i = 1; i <= n; i++) {
+//     sum = sum + i;
+//   }
 
-  return sum;
+//   return sum;
+// }
+
+// console.log(display(5));
+
+// 2.sum of digits of a number
+
+// function digits(n) {
+
+//     let result=JSON.stringify(n).split("")
+//     let sum=0
+// for(let i=0;i<result.length;i++){
+// sum+=Number(result[i])
+// }
+
+// return sum
+
+// }
+
+// console.log(digits(457))
+function digits(n) {
+
+  
+    
+
+
 }
 
-console.log(display(5));
+console.log(digits(457))
