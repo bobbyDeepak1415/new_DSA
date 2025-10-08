b6fc08ab4c4fc840d97ba042d2ac4a9f22df4d4d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-09 00:49:41 +0530

    third

--- index.js
+++ index.js
@@ -59,3 +59,26 @@
 
 // console.log(countDigits(-78093));
 // console.log(countDigits(278749093));
+
+// --------------------------------------------------------
+
+// 4.palindrome
+
+function display(num){
+
+    let copyNum=num,reverseNum=0
+
+
+
+    while(copyNum>0){
+        const lastDigit=copyNum%10
+        reverseNum=reverseNum*10+lastDigit
+        copyNum=Math.floor(copyNum/10)
+
+    }
+return num===reverseNum
+
+    
+}
+
+console.log(display(121))
