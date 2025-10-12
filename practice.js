ba11504576e1ea97d91a01d748bc0ff9d2326aa9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-13 02:11:26 +0530

    basics ques 2

--- practice.js
+++ practice.js
@@ -1,16 +1,42 @@
-
 // Basics questions
 
 // 1.count odd nums
 
-function display(high, low) {
-  let nums = 0;
+// function display(high, low) {
+//   let nums = 0;
 
-  for (let i = low; i <= high; i++) {
- if(i%2!=0) nums++
-  }
+//   for (let i = low; i <= high; i++) {
+//  if(i%2!=0) nums++
+//   }
 
-  return nums;
-}
+//   return nums;
+// }
 
-console.log(display(10, 8));
+// console.log(display(10, 8));
+
+// -------------------------------------------------------------------------------
+
+// 2.(FizzBuzz) Given an integer n, return a string array answer (1-indexed) where:
+
+// function display(n) {
+//   let arr = [];
+
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       arr.push("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       arr.push("Fizz");
+//     } else if (i % 5 === 0) {
+//       arr.push("Buzz");
+//     } else {
+//       arr.push(i.toString());
+//     }
+//   }
+
+//   return arr;
+// }
+
+// console.log(display(15));
+
+// -------------------------------------------------------------------------------------------
+
