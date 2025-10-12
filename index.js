240a7a7addc668114bd3fe9bca6dba4646f80b6e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-12 18:15:04 +0530

    flat

--- index.js
+++ index.js
@@ -267,8 +267,10 @@
 
 // *Flat
 
-const arr1 = [1,[2,[3,4,5,6],7,8],9,10]
+// const arr1 = [1,[2,[3,4,5,6],7,8],9,10]
 
-console.log(arr1.flat())
-console.log(arr1.flat(1))
-console.log(arr1.flat(2))
+// console.log(arr1.flat())
+// console.log(arr1.flat(1))
+// console.log(arr1.flat(2))
+
+// -------------------------------------------------------------------------------------
