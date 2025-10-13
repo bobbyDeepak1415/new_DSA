c26265e93975ccefeff13677cd6c9ca635f4a81d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-13 17:26:53 +0530

    basics ques 4

--- practice.js
+++ practice.js
@@ -68,3 +68,25 @@
 
 // Array questions
 
+// 1.Two sum
+
+const nums=[2, 7, 11, 15];
+
+function display(nums,target){
+
+    for(let i=0;i<nums.length;i++){
+
+        for(let j=i+1;j<nums.length;j++){
+
+            if(nums[i]+nums[j]===target) return nums[i],nums[j]
+
+        }
+
+          }
+    }
+
+
+
+
+// console.log(display())
+
