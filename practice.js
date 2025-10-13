dd27a3c7ec9cab0fbf70e0003338b1bbe388786c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-14 00:43:20 +0530

    Array ques 3

--- practice.js
+++ practice.js
@@ -114,8 +114,25 @@
 
 let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
-function display(){
+function display(nums) {
+    if (nums.length === 0) return 0;
+
+    let k=1
+
+    for(let i=1;i<nums.length;i++){
+        if(nums[i]!==nums[i-1]) {
+            nums[k]=nums[i]
+            k++
+        }
+    }
+
+    return k
+
+    
+    
+
+
 
 }
 
-console.log(display())
+console.log(display(nums),nums);
