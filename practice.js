da7ec4241b3aed4e52daa75c92b9d72af1c91282
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-13 23:57:06 +0530

    Array ques 2

--- practice.js
+++ practice.js
@@ -86,16 +86,29 @@
 
 // 2.Majority Element(Given an array nums of size n, return the majority element
 
- const nums = [2, 2, 1, 1, 1, 2, 2];
+const nums = [3, 2, 3];
 
 function display(nums) {
+  
+  
+    let count={}
+    let maxNum=nums[0]
+    let maxCount=0
+
+
+    for(let n of nums){
+        count[n]=(count[n])+1
+
+        if(count[n]>maxCount){
+            maxCount=count[n]
+            maxNum=n
+        }
+
+    }
+
+    return maxNum
 
     
-  
 }
 
 console.log(display(nums));
-
-
-
-
