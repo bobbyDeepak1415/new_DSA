fd425f561216c0a7539db28964c96ec5cb7f62a2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-18 03:31:58 +0530

    Strings started

--- Strings.jsx
+++ Strings.jsx
@@ -1,5 +1,5 @@
 
 
-const name1="Bobby Deepak is a front end developer.Also a man first"
+const name1="          Bobby Deepak is a front end developer.Also a man first          "
 
-console.log(name1.startsWith("Bobby"))
+console.log(name1.trimStart())
