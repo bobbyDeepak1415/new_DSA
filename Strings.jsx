41cadc52c723df2a99327a35f9af5d9f87abf64c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-18 00:56:01 +0530

    Strings started

--- Strings.jsx
+++ Strings.jsx
@@ -1,12 +1,5 @@
 
-// const arr1=  ['hello',"world","to","apple"]
-const arr1=  [45,87,4,9,12,6]
+const name1="Bobby"
 
-function display(arr1) {
 
-    let result=arr1.reduce((acc,num)=>acc+num)
-    return result
-  
-}
-
-console.log(display(arr1))
+console.log(name1.replace("bb","ll"))
