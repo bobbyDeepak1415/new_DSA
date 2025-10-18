4c4c68bd56b352b7728ba6806ee708a54234085d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-18 22:58:26 +0530

    Strings question 2

--- Strings.jsx
+++ Strings.jsx
@@ -2,25 +2,43 @@
 
 // 1.find Index of needle
 
-// let hayStack = "Bobby is a man";
-// let needle = "man";
+let hayStack = "Bobby is a man";
+let needle = "man";
 
-// function display(hayStack, needle) {
-//   return hayStack.indexOf(needle);
-// }
+function display(hayStack, needle){
 
-// console.log(display(hayStack, needle));
 
+
+  for (let i = 0; i < hayStack.length; i++) {
+    let found=true
+
+    for(let j=0;j<needle.length;j++){
+        if(hayStack[i+j]!==needle[j]){
+            found=false
+            break
+        }
+
+    }
+    if(found) return i
+  }
+
+  return -1
+}
+
+console.log(display(hayStack, needle));
 
 // ----------------------------------------------------------------------------
 
 // 2.reverse String
 
+// let s = ["h", "e", "l", "l", "o"];
 
-let s = ["h", "e", "l", "l", "o"]; 
+// function display(hayStack, needle) {
+//   return str.reverse()
+// }
 
-function display(hayStack, needle) {
-  return str.reverse()
-}
+// console.log(display(str));
 
-console.log(display(str));
+// -------------------------------------------------------------------------------
+
+// 3.valid anagram
