93a1de55b3ca157cdb1a23dbe60cd7a038a58b17
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-18 15:28:48 +0530

    Strings question 1

--- Strings.jsx
+++ Strings.jsx
@@ -1,4 +1,16 @@
-let firstName = "Bobby";
-let lasttName = "Deepak";
 
-console.log(`${firstName} ${lasttName}`);
+// Questions
+
+// 1.find Index of needle
+
+let hayStack = "Bobby is a man";
+let needle="man"
+
+
+function display(hayStack,needle){
+
+    return hayStack.firstindexof(needle)
+    
+}
+
+console.log(display(hayStack,needle))
