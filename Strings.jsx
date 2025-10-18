7a1d6ecb66c8b577d2ce4a2958243c202a11b8a9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-19 00:12:21 +0530

    Strings question 2

--- Strings.jsx
+++ Strings.jsx
@@ -2,30 +2,13 @@
 
 // 1.find Index of needle
 
-let hayStack = "Bobby is a man";
-let needle = "man";
-
-function display(hayStack, needle){
+let hayStack = "Bobby is a man and a Developer";
+let needle = "Developer";
 
 
-
-  for (let i = 0; i < hayStack.length; i++) {
-    let found=true
-
-    for(let j=0;j<needle.length;j++){
-        if(hayStack[i+j]!==needle[j]){
-            found=false
-            break
-        }
-
-    }
-    if(found) return i
-  }
-
-  return -1
+function display(){
+  
 }
-
-console.log(display(hayStack, needle));
 
 // ----------------------------------------------------------------------------
 
