37b38ec49c56b4f5049dda07bb59cafc738a2ffc
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-18 15:31:17 +0530

    Strings question 1

--- Strings.jsx
+++ Strings.jsx
@@ -1,16 +1,15 @@
-
 // Questions
 
 // 1.find Index of needle
 
-let hayStack = "Bobby is a man";
-let needle="man"
+// let hayStack = "Bobby is a man";
+// let needle = "man";
+
+// function display(hayStack, needle) {
+//   return hayStack.indexOf(needle);
+// }
+
+// console.log(display(hayStack, needle));
 
 
-function display(hayStack,needle){
-
-    return hayStack.firstindexof(needle)
-    
-}
-
-console.log(display(hayStack,needle))
+// ----------------------------------------------------------------------------
