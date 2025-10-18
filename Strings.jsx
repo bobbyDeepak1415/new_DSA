fbc9d5cf009ab0b5605ab7df56b325e4678985be
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-18 15:14:29 +0530

    Strings started

--- Strings.jsx
+++ Strings.jsx
@@ -1,8 +1,4 @@
+let firstName = "Bobby";
+let lasttName = "Deepak";
 
-const name1="Bobby"
-
-let arr1=["apples","grapes","oranges"]
-
-let name2="Deepak"
-
-console.log(arr1.concat(name2))
+console.log(`${firstName} ${lasttName}`);
