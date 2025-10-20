978242ebf62c8619254901d4428d9771aa60a15a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-20 23:22:07 +0530

    hashmap

--- Complexity.jsx
+++ Complexity.jsx
@@ -1,5 +1,3 @@
-
-
 // 1. O(n)
 
 // function display(nums){
@@ -10,3 +8,46 @@
 
 //     return sum
 // }
+
+// --------------------------------------------------------------------------
+
+// 2.O(n^2)
+
+// function display(nums){
+//     let sum=0
+//     for(let i=0;i<nums.length;i++){
+// for(let j=0;j<nums.length;j++){
+//     sum+=nums[i][j]
+// }
+//     }
+
+//     return sum
+// }
+
+// --------------------------------------------------------------------------------
+
+// 3.log(n)
+
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+// function display(nums) {
+//   let arr = [];
+//   for (let i = 1; i < nums.length; i = i * 2) {
+//     arr.push(nums[i]);
+//   }
+
+//   return arr;
+// }
+
+// console.log(display(nums));
+
+// how many times can you double 1 before it reaches n
+
+// 2^k<n
+// 2^k=n
+// k=logn
+
+
+// --------------------------------------------------------------------------------
+
