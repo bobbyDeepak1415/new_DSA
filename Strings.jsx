af0f953fe62f60b82c61bc30113d0e3a874303c8
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-21 17:36:15 +0530

    strings ques 6

--- Strings.jsx
+++ Strings.jsx
@@ -199,15 +199,15 @@
 
 // 6.length of last word
 
-// let str = "   fly me   to   the moon  ";
+let str = "   fly me   to   the moon  ";
 // let str1 = "hello world";
 
-// function display(str) {
+function display(str) {
 
-//   return str.trimEnd().split(" ").at(-1).length
+  return str.trimEnd().split(" ").at(-1).length
 
-// }
+}
 
-// console.log(display(str));
+console.log(display(str));
 
 
