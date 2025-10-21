4e0e9eb66c48b2cb11d152d133a705b9e433591d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-21 13:39:41 +0530

    strings ques 4

--- Strings.jsx
+++ Strings.jsx
@@ -145,62 +145,6 @@
 
 
 
-function sum(a,b){
-    return a+b
-}
-function sum2(a,b,c){
-    return a+b+c
-}
-
-function product(a,b,c){
-    return a*b*c
-}
-
-function getUniqueId(fn,args){
-
-    let uniqueId=[]
-
-    uniqueId=uniqueId.concat(args).join("|")
-    return uniqueId
-
-}
-
-
-function memoize(fn){
-
-    let cache={}
-
-    return function (...args){
-        let uniqueId=getUniqueId(fn,args)
-
-        if(cache[uniqueId]) {
-            console.log("fron cache",cache[uniqueId])
-
-        }
-
-     cache[uniqueId]=fn(...args)
-     console.log("not from cache")
-        return cache[uniqueId]
-
-
-    }
-
-    
-
-}
-
-
-
-
-const result=memoize(sum)
-const result2=memoize(sum2)
-const result1=memoize(product)
-
-console.log(result(2,3))
-console.log(result1(2,3,3))
-console.log(result1(2,3,3))
-console.log(result2(2,3,3))
-
 
 
 
