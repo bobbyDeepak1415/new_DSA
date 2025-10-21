839bed5e6c241f81fc3af485ebdeccf678e3cb46
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-21 13:23:02 +0530

    strings ques 4

--- Strings.jsx
+++ Strings.jsx
@@ -124,12 +124,22 @@
 
 let strs = ["flower", "flow", "flight"];
 
-// function display(strs) {
+function display(strs) {
+  if (strs.length === 0) return;
 
-// }
+  let prefix = strs[0];
 
-console.log((strs));
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1);
+    }
+
+    if (prefix.length === 0) return "";
+  }
+
+  return prefix;
+}
+
+console.log(display(strs));
 
 // -------------------------------------------------------------------------------
-
-
