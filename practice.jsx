46e32e33bfa4a4ca5392329ee93bd4d96d82f102
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 02:21:31 +0530

    index 2

--- practice.jsx
+++ practice.jsx
@@ -1,16 +1,16 @@
 // 3. count the number of digits in a number
 
+function countDigits(num) {
 
-function countDigits(num){
+    num=Math.abs(num)
 
-    let result=JSON.stringify(Math.abs(num)).split("")
+  let count = 0;
+  do {
+    count++;
+    num = Math.floor(num / 10);
+  } while (num > 0);
 
-    return result.length
-
-
-
+  return count;
 }
 
-
-
-console.log(countDigits(8932789903973))
+console.log(countDigits(-8093));
