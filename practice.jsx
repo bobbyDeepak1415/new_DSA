ec9799f2ba0d9b3fc0cc963e88b241f606b28364
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 02:18:07 +0530

    index 2

--- practice.jsx
+++ practice.jsx
@@ -1,14 +1,9 @@
 // 3. count the number of digits in a number
 
 
-function display(n){
+function countDigits(num){
 
-    let arr1=JSON.stringify(Math.abs(n)).split("")
-    return arr1.length
 
-   
+    
+
 }
-
-
-
-console.log(display(-9199088))
