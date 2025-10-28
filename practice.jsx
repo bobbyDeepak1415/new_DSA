84460befe8f763ffd399fe7b52c7c8fd106de24c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 03:03:08 +0530

    index 4

--- practice.jsx
+++ practice.jsx
@@ -1,10 +1,11 @@
 // 4.palindrome
 
-function countDigits(num) {
-
+function display(num) {
 
     
+  let rev = Number(JSON.stringify(Math.abs(num)).split("").reverse().join(""));
 
+  return Math.abs(num) === rev;
 }
 
-
+console.log(display(-89898));
