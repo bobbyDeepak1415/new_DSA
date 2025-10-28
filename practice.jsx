d51cb97a6b66ae14d111f6893de02d7334920b61
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 01:58:28 +0530

    index 1

--- practice.jsx
+++ practice.jsx
@@ -1,1 +1,18 @@
-console.log("hellollllll")
+// 1. sum of 1 to n
+
+
+function display(n){
+
+
+    let sum=0
+    for(let i=0;i<=n;i++){
+        sum+=i
+    }
+
+    return sum
+
+}
+
+
+
+console.log(display(5))
