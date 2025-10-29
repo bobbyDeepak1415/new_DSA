cc2d60689bcc54a8d1b9c4d22860ef5ed70b46c1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 17:07:21 +0530

    index 5

--- practice.jsx
+++ practice.jsx
@@ -1,27 +1,9 @@
-// 4.palindrome
+// 5.fibonacci
 
-function display(num) {
-  let rev = 0;
-  //   let local = Math.abs(num);
-  let local = Math.abs(num);
-  while (local > 0) {
-    rev = rev * 10 + (local % 10);
-    local = Math.floor(local / 10);
-  }
 
-  return rev === Math.abs(num);
+function display(){
+
 }
 
-console.log(display(-6776));
+console.log(display())
 
-// 1
-// rev=0*10+7=7
-// num=67
-
-// 2
-// rev=7*10+7=77
-// num=6
-
-// 1
-// rev=77*10+6
-// num=0
