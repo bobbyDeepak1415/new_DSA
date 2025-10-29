f20cf60dd2eb4c3bb0fef28c77bb64ab86947cc2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 23:23:32 +0530

    index 7

--- practice.jsx
+++ practice.jsx
@@ -1,18 +1,12 @@
 // 7.number present or not
 
-let arr1 = [12, 2, 33, 41, 52, 6, 71, 80, 9, 10];
+let arr1 = [12, 2, 33];
+let arr2 = [12, 2, 33];
 
-// delete and update
 
-// function display(num,arr){
+function display(arr1,arr2){
 
-//   [2,33,41,52]
+}
 
-// }
+console.log(arr1,arr2)
 
-console.log(arr1);
-
-const result = arr1.slice(1, 5);
-
-console.log(result);
-// ...
