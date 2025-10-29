3acf4a849998e684131d83e2d90f579ffd70155a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 17:47:59 +0530

    index 6

--- practice.jsx
+++ practice.jsx
@@ -3,8 +3,6 @@
 let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10];
 
 function display(arr) {
-  let sum = arr.reduce((a, b) => a + b, 0);
-
-  return arr.length * (arr.length + 1)/2-sum
+    
 }
 console.log(display(arr1));
