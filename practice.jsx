f46c99b7b9c9696db3418f112b85d02df2021e4d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 16:59:02 +0530

    index 4

--- practice.jsx
+++ practice.jsx
@@ -1,1 +1,14 @@
 // 4.palindrome
+
+function display(num) {
+  let rev = 0;
+  let local = Math.abs(num);
+  while (num > 0) {
+    rev = rev * 10 + (local % 10);
+    num = Math.floor(num);
+  }
+
+  return rev === num;
+}
+
+console.log(display(677));
