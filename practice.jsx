17df3676e25e5081b187857ad6ab48bf5923a2c4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-30 02:48:52 +0530

    map,reduce,filter

--- practice.jsx
+++ practice.jsx
@@ -1,7 +1,7 @@
 // 4.map,reduce,filter
 
-let arr1 = [12, -21, 4, -1, 9, 1, -2, 33];
+let arr1 = [12, [21, 4], 1, 9, [1, 2, [33,7,44],42,24]];
 
-const result=arr1.map((num,index)=>num*num)
+
 
 console.log(result);
