8044534533041f597489256fcb5ef9026bd358df
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 17:30:56 +0530

    index 5

--- practice.jsx
+++ practice.jsx
@@ -4,20 +4,20 @@
 // 0,1,2,3,4,5,6,7
 
 function display(num) {
+  if (num === 0) return 0;
+  if (num === 1) return 1;
 
-    if(num===0) return 0
-    if(num===1) return 1
+  let current = 1;
+  let next = 0;
+  let prev = 0;
 
+  for (let i = 2; i <= num; i++) {
+      next = current + prev;
+      prev = current;
+    current = next;
+  }
 
-    let current=1
-    let next=0
-    let prev=0
-
-    for()
-
-
-
+  return next;
 }
 
-
-console.log(display(7))
+console.log(display(7));
