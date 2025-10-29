66f65616d1c491883313f94afb51546091d436b9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 18:04:06 +0530

    index 6

--- practice.jsx
+++ practice.jsx
@@ -1,17 +1,7 @@
 // 6.missing number
 
-let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8,9, 10];
+let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function display(arr) {
-
-    let sum=0
-    for(let i=0;i<arr.length;i++){
-        sum+=arr[i]
-    }
-
-    return (arr.length*(arr.length+1))/2-sum
-
-
-
-}
+function display(arr) {}
 console.log(display(arr1));
+// done
