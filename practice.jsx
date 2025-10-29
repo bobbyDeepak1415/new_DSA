107ac7b618f85abce6519f617fb3f4f782a2c0b8
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 22:38:04 +0530

    index 6

--- practice.jsx
+++ practice.jsx
@@ -1,7 +1,18 @@
 // 6.missing number
 
-let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function display(arr) {}
-console.log(display(arr1));
-// done
+
+function display(num){
+
+  let sum=0
+
+  while(num>0){
+    sum+=num%10
+    num=Math.floor(num/10)
+
+  }
+  return sum
+}
+
+console.log(display(45792))
