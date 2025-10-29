18372a762d512856804703d8f2a8b835c3cde488
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 17:03:43 +0530

    index 4

--- practice.jsx
+++ practice.jsx
@@ -5,10 +5,22 @@ function display(num) {
   let local = Math.abs(num);
   while (num > 0) {
     rev = rev * 10 + (local % 10);
-    num = Math.floor(num);
+    num = Math.floor(num / 10);
   }
 
   return rev === num;
 }
 
-console.log(display(677));
+console.log(display(6776));
+
+// 1
+// rev=0*10+7=7
+// num=67
+
+// 2
+// rev=7*10+7=77
+// num=6
+
+// 1
+// rev=77*10+6
+// num=0
