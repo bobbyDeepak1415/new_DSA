28612b75af7fd62f4c63a4ef4f3b510a20caf936
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-29 17:44:42 +0530

    index 6

--- practice.jsx
+++ practice.jsx
@@ -1,14 +1,10 @@
-// 5.missing number
+// 6.missing number
 
-let arr1=[0,1,2,3,4,5,6,7,8,10]
+let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10];
 
 function display(arr) {
+  let sum = arr.reduce((a, b) => a + b, 0);
 
-    let sum=arr.reduce((a,b)=>a+b,0)
-
-    return (arr.length*arr.length+1)
-
-
-
+  return arr.length * (arr.length + 1)/2-sum
 }
 console.log(display(arr1));
