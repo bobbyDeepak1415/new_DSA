23cc5a62066ee72db18cf2a4a4124b4d200206f2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-30 02:29:13 +0530

    index 3

--- practice.jsx
+++ practice.jsx
@@ -4,17 +4,9 @@ let arr1 = [12, 21, 33];
 let arr2 = [12, 21, 33];
 
 function display(arr1, arr2) {
-
-    if(arr1.length!==arr2.length) return false
-
-    for(let i=0;i<arr1.length;i++){
-        if(arr1[i]!==arr2[i]) return false
-    }
-
-    return true
-
-
-
+  return (
+    arr1.length === arr2.length && arr1.every((ele, i) => arr1[i] === arr2[i])
+  );
 }
 
-console.log(display(arr1,arr2));
+console.log(display(arr1, arr2));
