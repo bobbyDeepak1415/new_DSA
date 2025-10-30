b677d811a93d6719120bb820a808a6710985dd18
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-31 00:24:07 +0530

    odd nums

--- practice.jsx
+++ practice.jsx
@@ -3,7 +3,14 @@
 function display(num) {
 
     let arr1=[]
-    
+
+    for(let i=0;i<=num;i++){
+        if(i%3===0 && i%5===0) arr1.push("FizzBuzz")
+       else if(i%3===0 ) arr1.push("Fizz")
+       else if(i%5===0) arr1.push("Buzz")
+       else  arr1.push(i)
+    }
+return arr1
 
  
 }
