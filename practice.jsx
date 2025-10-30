8168872266327153a9904e2d805d517f7cf6c0d1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-30 23:58:19 +0530

    Fizz Buzz

--- practice.jsx
+++ practice.jsx
@@ -1,16 +1,14 @@
 // 1.fizzNBuzz
 
-let arr1 = [5, 15, 25, 30, 45, 35, 3];
-function display(arr1) {
-  let local = [];
+// let arr1 = [5, 15, 25, 30, 45, 35, 3];
+function display(num) {
+    
+    if (num % 3 === 0 && num % 5 === 0) return "FizzBuzz";
+    if (num % 3 === 0) return "Fizz"
+    if (num % 5 === 0) return "Buzz"
 
-  for (let i = 0; i < local.length; i++) {
-    if (i % 3 === 0) local.push("Fizz");
-    if (i % 5 === 0) local.push("Buzz");
-    if (i % 3 === 0 && i % 5 === 0) arr1.push("FizzBuzz");
-  }
+    else return "false"
 
-  return local;
 }
 
-console.log(display(arr1));
+console.log(display(45));
