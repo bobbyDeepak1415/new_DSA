d9fbaf8aa65b4236650910199ebab7a2a4467d17
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-30 23:50:53 +0530

    odd nums

--- practice.jsx
+++ practice.jsx
@@ -1,2 +1,14 @@
-// 4.go to basics and arrays
+// 1.count odd nums
+function display(low, high) {
+  
+    let count=0
+    for(let i=low;i<=high;i++){
+        if(i%2!==0) count++
+    }
 
+    return count
+
+
+}
+
+console.log(display(1, 10));
