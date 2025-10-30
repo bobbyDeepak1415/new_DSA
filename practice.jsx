03884772e541edefb0f8c16b1aa5775cca504848
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-31 02:42:42 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,17 +1,13 @@
 // 1.array questions
 
-let arr1=[3,8,5,9,2,4]
+const arr1 = [3, 8, 5, 9, 2, 4];
 
-function display(arr1,num){
-
-    for(let i=0;i<arr1.length;i++){
-        for(let j=i+1;j<arr1.length-1;i++){
-            if(arr1[i]+arr1[j]===num) return [i,j]
-        }
+function display(arr1, num) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = i + 1; j < arr1.length; j++) {
+      if (arr1[i] + arr1[j] == num) return [i, j];
     }
-
-return 
-
+  }
 }
 
-// console.log(display(arr1,9))
+console.log(display(arr1, 9));
