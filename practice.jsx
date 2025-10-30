a58b6ad2dddd4c36a244f3b0ff90013b8b53bc24
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-31 00:35:38 +0530

    odd nums

--- practice.jsx
+++ practice.jsx
@@ -1,13 +1,2 @@
-// 1.sqrt
+// 1.array questions
 
-// function display(num) {
-//   return num > 0 && (num & (num - 1)) === 0;
-// }
-
-// console.log(display(64));
-
-function display(num) {
-  return Math.floor(Math.sqrt(num))
-}
-
-console.log(display(85));
