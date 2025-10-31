e87458f174e93841bdd6f1ed2229dcb628e56ded
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 02:43:32 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,5 +1,27 @@
 // 1.hashmap
 
-function display(arr1) {}
+let str = "{{{([[]){}}}";
+let str1 = "{[{[[]]}]}";
 
-// console.log(display(arr1));
+function display(str) {
+  const map = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+  };
+
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (["{", "[", "("].includes(char)) {
+      stack.push(char);
+    } else if (["}", "]", ")"].includes(char)) {
+      if (stack.pop() !== map[char]) return false;
+    }
+  }
+
+  return stack.length===0
+}
+
+console.log(display(str1));
