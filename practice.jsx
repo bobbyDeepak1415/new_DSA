f7a7127242ccd7e1c0993f726ca39f8b26cf5ffc
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-31 17:58:11 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,11 +1,11 @@
 // 1.two sum
 
-const arr1 = [3, 8, 5, 9, 2, 4];
+const arr1 = [8, 1, 3, 8, 5, 9, 2, 4];
 
 function display(arr1, num) {
   for (let i = 0; i < arr1.length; i++) {
     for (let j = i + 1; j < arr1.length; j++) {
-      if (arr1[i] + arr1[j] == num) return [i, j];
+      if (arr1[i] + arr1[j] === num) return [i, j];
     }
   }
 }
