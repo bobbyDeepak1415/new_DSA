18e22ace6df94395dd335a688189dbe713d479fb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 02:45:35 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -4,24 +4,5 @@ let str = "{{{([[]){}}}";
 let str1 = "{[{[[]]}]}";
 
 function display(str) {
-  const map = {
-    "}": "{",
-    "]": "[",
-    ")": "(",
-  };
-
-  const stack = [];
-
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (["{", "[", "("].includes(char)) {
-      stack.push(char);
-    } else if (["}", "]", ")"].includes(char)) {
-      if (stack.pop() !== map[char]) return false;
-    }
-  }
-
-  return stack.length===0
 }
-
 console.log(display(str1));
