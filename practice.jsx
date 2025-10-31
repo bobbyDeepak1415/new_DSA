b27b353ef40880aa08eb7d62241b450fa5f4857a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-31 18:06:27 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,13 +1,5 @@
-// 1.two sum
+// 1.hashmap
 
-const arr1 = [8, 1, 3, 8, 5, 9, 2, 4];
+function display(arr1) {}
 
-function display(arr1, num) {
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = i + 1; j < arr1.length; j++) {
-      if (arr1[i] + arr1[j] === num) return [i, j];
-    }
-  }
-}
-
-console.log(display(arr1, 9));
+// console.log(display(arr1));
