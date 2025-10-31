3fd6048661054c0c0ea6cead9988467880ca2596
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 02:54:26 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -4,5 +4,18 @@ let str = "{{{([[]){}}}";
 let str1 = "{[{[[]]}]}";
 
 function display(str) {
+  let map = { "}": "{", "]": "[", ")": "(" };
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (["{", "[", "("].includes(str[i])) {
+      stack.push(str[i]);
+    } else if (["}", "]", ")"].includes(str[i])) {
+      if (stack.pop() !== map[str[i]]) return false;
+    }
+  }
+
+  return stack.length===0
 }
-console.log(display(str1));
+console.log(display(str));
