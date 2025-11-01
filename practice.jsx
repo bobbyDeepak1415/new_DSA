c03789548894e4b572e11680e17bef7c345537ea
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 20:47:30 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,12 +1,13 @@
 // 1.square of elements
 
-let arr1 = [3,  4, 3, 5];
+let arr1 = [3, 4, -3, 2, 5, -1];
 
 function display(arr1) {
+  for (let i = 0; i < arr1.length; i++) {
+    arr1[i] = arr1[i] * arr1[i];
+  }
 
-    for(let i=0)
-
-
+  return arr1.sort((a,b)=>a-b)
 
 }
 console.log(display(arr1));
