ad86b0e9e2a1c18adc61953bf5571bbc8cd7653a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 21:03:05 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,13 +1,19 @@
-// 1.square of elements
+// 1.pivot element
 
-let arr1 = [3, 4, -3, 2, 5, -1];
+let arr1 = [6, 6, 3, 1, 4, 4, 3];
 
 function display(arr1) {
-  for (let i = 0; i < arr1.length; i++) {
-    arr1[i] = arr1[i] * arr1[i];
-  }
 
-  return arr1.sort((a,b)=>a-b)
+    let sum=arr1.reduce((a,b)=>a+b)
+
+    let leftSum=0
+
+    for(let i=0;i<arr1.length;i++){
+        
+    }
+
+
+
 
 }
-console.log(display(arr1));
+// console.log(display(arr1));
