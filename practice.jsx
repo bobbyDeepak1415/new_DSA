4c5cec22d27d66823128cca0222b09c1250a4e96
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 03:40:16 +0530

    odd nums

--- practice.jsx
+++ practice.jsx
@@ -1,9 +1,9 @@
 // 8.maximum no. of consequtive 1s
 
-let arr1 = [1, 0, 1, 1, 0, 1];
+let arr1 = [1, 1, 0, 1,9,1,1,1,1];
 
 function display(arr) {
-  let k = 0;
+  
 }
 
 console.log(display(arr1));
