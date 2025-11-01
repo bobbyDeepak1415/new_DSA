914c4633bd85cabee2bf8bae141df5b82edf4fed
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 13:04:54 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,9 +1,14 @@
 // 1.hashmap
 
 
-let arr1=[4,4,3,3,3,3,5,6,5,5,5,5]
+// let arr1=[4,4,3,3,3,3,5,6,5,5,5,5]
+
+let str="{{{[({})]}}}"
+let str1="{{}{[({})]}}}"
 
 function display(arr) {
+
+
 
 }
 // console.log(display(arr1));
