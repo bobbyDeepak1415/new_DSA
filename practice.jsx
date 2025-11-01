8e24f3ddd7b2682efcef11e7ee192625b85a3626
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 03:51:11 +0530

    odd nums

--- practice.jsx
+++ practice.jsx
@@ -1,9 +1,25 @@
-// 8.maximum no. of consequtive 1s
+// 8.majority element
 
-let arr1 = [1, 1, 0, 1,9,1,1,1,1];
+let arr1 = [1, 1, 0, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1, 1];
 
 function display(arr) {
-  
+  let count = {};
+  let maxCount = 0;
+  let maxNum = arr[0];
+
+  for (let n of arr) {
+    count[n] = (count[n] || 0) + 1;
+
+    if (count[n] > maxCount) {
+      maxCount = count;
+      maxNum = n;
+    }
+  }
+
+  return maxNum;
+
+
+
 }
 
 console.log(display(arr1));
