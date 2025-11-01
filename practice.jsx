6120bdb4cb58350ab6bdf1bbfa38cab43941d96e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 20:44:20 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -4,17 +4,6 @@ let arr1 = [3,  4, 3, 4, 4, 4, 5];
 
 function display(arr1) {
 
-    let k=1
-
-    for(let i=0;i<arr1.length;i++){
-        if(arr1[i]!==arr1[i-1]){
-            arr1[k]=arr1[i]
-            k++
-        }
-    }
-
-    return k
- 
 
 
 }
