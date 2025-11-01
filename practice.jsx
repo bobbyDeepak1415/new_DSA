77b556abd6517b7e1d15cda0964d7f0f22e849bb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 19:56:57 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,18 +1,22 @@
 // 1.most occuring number
 
-
-let arr1=[4,4,5,3,3,5,3,5,3,5,6,5,5]
-
-
-
-
+let arr1 = [4, 4, 5, 4, 3, 3, 4, 3, 3, 5, 6, 5, 5, 4, 4, 4];
 
 function display(arr1) {
+  let count = {};
 
-    
-   
+  let maxNum = arr1[0];
+  let maxCount = 0;
 
+  for (let n of arr1) {
+    count[n] = (count[n] || 0) + 1;
 
+    if (count[n] > maxCount) {
+      maxCount = count[n];
+      maxNum = n;
+    }
+  }
 
+  return maxNum;
 }
 console.log(display(arr1));
