c058314ddacf7e2a98928d43447aa0f1886b718f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 02:29:38 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -3,10 +3,20 @@
 let arr1 = [5, 6, 0, 0, 4, 15, 0, 9];
 
 function display(arr) {
-  
+  let k = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      arr[k] = arr[i];
+      k++;
+    }
+  }
 
+  for (let i = k; i < arr.length; i++) {
+    arr[i] = 0;
+  }
 
+  return arr;
 }
 
-// console.log(display(arr1));
+console.log(display(arr1));
