87c55b31536356b9bdf725d929857fd73e02ab80
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 13:48:42 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,34 +1,17 @@
 // 1.hashmap
 
 
-// let arr1=[4,4,3,3,3,3,5,6,5,5,5,5]
+let arr1=[4,4,3,3,3,3,5,6,5,5,5,5]
 
 
 
-let str="{{{[({})]}}}"
-let str1="{{}{[({})]}}}"
 
 
-function display(str) {
+function display(arr1) {
 
-    let map = { "}": "{", "]": "[", ")": "(" };
-
-    let cache=[]
-
-    for(let i=0;i<str.length;i++){
-        let char=str[i]
-        if(["{","[","("].includes(char)){
-            cache.push(char)
-        }else if(["}","]",")"].includes(char)){
-            // if(cache.pop()!==map[char]) return false
-        }
-
-    }
-
-    return cache.length===0
-
+   
 
 
 
 }
-console.log(display(str1));
+console.log(display(arr1));
