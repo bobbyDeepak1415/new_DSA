22a354f9d2f887fbb509c9164bc4a0d081c9aac7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 13:15:25 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -3,14 +3,33 @@
 
 // let arr1=[4,4,3,3,3,3,5,6,5,5,5,5]
 
+
+
 let str="{{{[({})]}}}"
 let str1="{{}{[({})]}}}"
 
-function display(arr) {
+console.log(str)
+
+// function display(arr) {
+    
 
 
-
-}
+// }
 // console.log(display(arr1));
 
+// 0,1,1,2,3,5,8
+// 0,1,2,3,4,5,6
 
+
+
+ function display(num){
+
+    if(num===0) return 0
+    if(num===1) return 1
+
+
+    
+
+
+ }
+console.log(display(6))
