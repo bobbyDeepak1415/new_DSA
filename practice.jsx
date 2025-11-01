f8c4ac01303ccc7610ee944caa4d3cbce66ba16a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-01 21:09:03 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -2,20 +2,9 @@
 
 let arr1 = [6, 6, 3, 1, 4, 4, 3];
 
-function display(arr1) {
-  let sum = arr1.reduce((a, b) => a + b);
-  let leftSum = 0;
 
-  for (let i = 0; i < arr1.length; i++) {
-    let rightSum = sum - leftSum - arr1[i];
+function display(){
 
-    if (leftSum === rightSum) {
-      return i;
-    }
-
-    leftSum += arr1[i];
-  }
-
-  return -1;
 }
-console.log(display(arr1));
+
+// console.log(display())
