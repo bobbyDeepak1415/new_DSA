4ba7ea8c750f84a03fc84e3df26e580b7004b68b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 00:26:50 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,49 +1,25 @@
 // 5.move all zeroes to left
 
-// let arr1 = [5, 6, 0, 0, 4, 0];
+let arr1 = [5, 6, 0, 0, 4, 0];
 
-// function display(arr) {
+function display(arr) {
 
-//     let k=0
+    let k=0
 
-//     for(let i=0;i<arr.length;i++){
+    for(let i=0;i<arr.length;i++){
 
-//         if(arr[i]===0){
-//             arr1[k]=arr[i]
-//             k++
-//         }
+        if(arr[i]===0){
+            arr1[k]=arr[i]
+            k++
+        }
 
-//     }
+    }
 
-//     return arr
+    return arr
   
 
 
-// }
+}
 
-// console.log(display(arr1));
+console.log(display(arr1));
 
-
-/ let nums = [0, 1, 0, 3, 12];
-
-// function display(nums) {
-
-//     let k=0
-
-//     for(let i=0;i<nums.length;i++){
-//         if(nums[i]!==0){
-//             nums[k]=nums[i]
-//             k++
-//         }
-
-//     }
-
-//     for(let i=k;i<nums.length;i++){
-//         nums[i]=0
-//     }
-
-//     return nums
-
-// }
-
-// console.log(display(nums));
