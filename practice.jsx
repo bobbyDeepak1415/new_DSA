40732fdba6edb1c9b9618dac37aaacecc7501bba
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-02 03:12:51 +0530

    two sum

--- practice.jsx
+++ practice.jsx
@@ -1,12 +1,12 @@
 // 5.remove element and give the length of the non value elements in the array
 
-let arr1 = [16, 16, 0, 0, 15, 0, 9,16];
+let arr1 = [0, 1, 2, 2,5, 3, 0, 4, 2];
 
-function display(arr,value) {
+function display(arr, value) {
   let k = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== value) {
-      arr[k] = arr[i];
+      arr[k]=arr[i]
       k++;
     }
   }
@@ -14,4 +14,4 @@ function display(arr,value) {
   return k;
 }
 
-console.log(display(arr1,16));
+console.log(display(arr1, 2));
