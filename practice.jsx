4570d16cf03c8bec58c80c42fd347339a601c57d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-05 01:34:50 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,23 +1,31 @@
 // 8.majority element
 
 
-let str="success"
+let str="{[[{({[]})}]]}"
+let str1="{[[}{({[}]})}]]}"
 
 
-function display(){
+function display(str){
 
-  let obj={}
+  let map = { "}": "{", ")": "(", "]": "[" };
+  let stack=[]
 
-  for(let i=0;i<=str.length;i++){
-    ;let char=str[i]
-
-    if(obj[char]) return char
-    else{
-      obj[char]=true
+  for(let i=0;i<str.length;i++){
+    let char=str[i]
+    if(["{","[","("].includes(char)){
+stack.push(char)
+    }else if(["}","]",")"].includes(char)){
+      if(stack.pop()!==map[char]){
+        return false
+      }
+    }else{
+      return true
     }
-
   }
-return null
+
+  return stack.length===0
+
+
 }
 
 console.log(display(str))
