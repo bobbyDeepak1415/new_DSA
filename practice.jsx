d38eb7b1b2c4685f6f4ed23ce8748d9d3b7570d5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-05 01:25:25 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,22 +1,11 @@
 // 8.majority element
 
-let nums = [ 1, 1, 1, 2, 2, 3];
 
-function display(nums) {
-
-  let k=1
-
-  for(let i=1;i<nums.length;i++){
-    if(nums[i]!==nums[i-1]){
-      nums[k]=nums[i]
-      k++
-    }
-  }
-
-  return nums.splice(0,k)
+let str="success"
 
 
+function display(){
 
 }
 
-console.log(display(nums)); // [0, 1, 2, 3, 4]
+// console.log(display(str))
