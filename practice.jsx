65a967a1a6e2c9d6530d1fc9e4b0d59d47620b2f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-05 01:28:40 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -6,6 +6,18 @@ let str="success"
 
 function display(){
 
+  let obj={}
+
+  for(let i=0;i<=str.length;i++){
+    ;let char=str[i]
+
+    if(obj[char]) return char
+    else{
+      obj[char]=true
+    }
+
+  }
+return null
 }
 
-// console.log(display(str))
+console.log(display(str))
