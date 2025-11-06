df86fb1e7e9fa13dc0f3f0dd32ea944fa0f969de
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-07 02:27:34 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,11 +1,21 @@
 // 8.majority element
 
-let str = "Bobby";
+let str1 = "anagram";
+let str2 = "nagaram";
 
-function display(str) {
+function display(str1, str2) {
+  // if(str1.length!==str2.length) return
 
-  return str.split("").reverse().join("")
-  
+  // let cache={}
+
+  // for(let i=0;i<str1.length;i++){
+
+  // }
+
+  const sort1 = [...str1].sort((a, b) => a.localeCompare(b)).join("");
+  const sort2 = [...str2].sort((a, b) => a.localeCompare(b)).join("");
+
+  return sort1 == sort2;
 }
 
-console.log(display(str));
+console.log(display(str1, str2));
