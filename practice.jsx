825af13ba75015365215d1c6f9a90bc14ea77f5f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-07 02:55:59 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -4,10 +4,21 @@ let str1 = "anagram";
 let str2 = "nagaram";
 
 function display(str1, str2) {
-  
+  if (str1.length !== str2.length) return false;
 
-  
+  let cache = {};
 
+  for (let i = 0; i <= str1.length; i++) {
+    let char = str1[i];
+    cache[char] = (cache[char] || 0) + 1;
+  }
+
+  for (let i = 0; i <= str2.length; i++) {
+    let char = str2[i];
+    if (!cache[char]) return false;
+    cache[char]--;
+  }
+  return true;
 }
 
 console.log(display(str1, str2));
