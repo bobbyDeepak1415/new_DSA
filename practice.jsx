db0281afc24b9fb3668abf7538d8f516e8ee6468
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-07 02:50:46 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -4,18 +4,10 @@ let str1 = "anagram";
 let str2 = "nagaram";
 
 function display(str1, str2) {
-  // if(str1.length!==str2.length) return
+  
 
-  // let cache={}
+  
 
-  // for(let i=0;i<str1.length;i++){
-
-  // }
-
-  const sort1 = [...str1].sort((a, b) => a.localeCompare(b)).join("");
-  const sort2 = [...str2].sort((a, b) => a.localeCompare(b)).join("");
-
-  return sort1 == sort2;
 }
 
 console.log(display(str1, str2));
