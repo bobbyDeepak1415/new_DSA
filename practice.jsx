0082daf9516d605f6b0a3613311ca8f67224f44f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-07 03:04:56 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,24 +1,21 @@
 // 8.majority element
 
-let str1 = "anagram";
-let str2 = "nagaram";
+let str = "apple is a fruit";
 
-function display(str1, str2) {
-  if (str1.length !== str2.length) return false;
+function display(str, needle) {
+  for (let i = 0; i <= str.length - needle.length; i++) {
+    let found = true;
 
-  let cache = {};
-
-  for (let i = 0; i <= str1.length; i++) {
-    let char = str1[i];
-    cache[char] = (cache[char] || 0) + 1;
+    for (let j = 0; j < needle.length; j++) {
+      if (str[i + j] !== needle[j]) {
+        found = false;
+        break;
+      }
+    }
+    if (found) return i;
   }
 
-  for (let i = 0; i <= str2.length; i++) {
-    let char = str2[i];
-    if (!cache[char]) return false;
-    cache[char]--;
-  }
-  return true;
+  return -1;
 }
 
-console.log(display(str1, str2));
+console.log(display(str, "fruit"));
