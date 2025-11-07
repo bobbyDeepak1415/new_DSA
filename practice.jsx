cb010f6ebebfa93a3872e88db5a2f669dafa2739
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-07 17:20:25 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,13 +1,16 @@
 // 8.majority element
 
 
-function counter(){
-  let count=0
-  return function(){
-    return ++count
-  }
-}
+// function counter(){
+//   let count=0
+//   return function(){
+//     return ++count
+//   }
+// }
 
 
-const increment=counter()
-console.log(increment())
+// const increment=counter()
+// console.log(increment())
+// console.log(increment())
+
+
