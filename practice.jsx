196303b5729b5718815d9e77cb3e0514dc0811e1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-09 23:34:09 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,19 +1,5 @@
-// 8.majority element
-
 let str = "the line has the largest";
 
-function display(str) {
-  let largestWord = "";
-  let words = str.split(" ");
+function display(str) {}
 
-  for (let i = 0; i < words.length; i++) {
-    let word = words[i];
-    if (word.length > largestWord.length) {
-      largestWord = word;
-    }
-  }
-
-  return largestWord;
-}
-
-console.log(display(str));
+// console.log(display(str));
