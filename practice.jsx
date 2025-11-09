f55521d602e70533f7ef947864539890421f05d7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-09 21:35:10 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -2,18 +2,23 @@
 
 let str = "the line has the largest word";
 
-function display(str) {
-  let largest = "";
-  let words = str.split(" ");
+function display(str,needle) {
 
-  for (let i = 0; i < words.length; i++) {
-    let word = words[i];
 
-    if (word.length > largest.length) {
-      largest = word;
+    for(let i=0;i<str.length-needle.length;i++){
+        let found=true
+
+        for(let j=0;j<needle.length;j++){
+            if(str[i+j]!==needle[j]){
+                found=false
+            }
+        }
+
+        if(found) return i
+
     }
-  }
 
-  return largest;
+    return -1
+  
 }
-console.log(display(str));
+console.log(display(str,"line"));
