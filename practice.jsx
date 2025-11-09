0a1374790cd60d0a6787308b5516109a58fa9aa7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-09 21:41:14 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,22 +1,14 @@
 // 8.majority element
 
-
 let str1 = "   fly me   to   the moon  ";
 let str2 = "hello worlds";
 
-
-
 function display(str) {
 
- return str.trim().split(" ")
+    
+  return str.trimEnd().split(" ").at(-1).length;
 
-//  return arr[arr.length-1].length
-
-
-
-
-
-
+  //  return arr[arr.length-1].length
 }
 
 console.log(display(str1));
