e15cb2ebf2f9dfe65e405b8cad6c83542b156a38
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-09 23:22:00 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,34 +1,15 @@
 // 8.majority element
 
-let str1 = "paal";
-let str2 = "alap";
-// function display(str1, str2) {
-//   return (
-//     [...str1].sort((a, b) => a.localeCompare(b)) ===
-//     [...str2].sort((a, b) => a.localeCompare(b))
-//   );
-// }
+let str1 = "anagram";
+let str2 = "nagaram";
+
 
 function display(str1, str2) {
-  let obj = {};
-
-  if (str1.length !== str2.length) return false;
-
-  for (let i = 0; i < str1.length; i++) {
-    let char = str1[i];
-    obj[char] = (obj[char] || 0) + 1;
-  }
-
-  for (let i = 0; i < str2.length; i++) {
-    let char = str2[i];
-    if (!obj[char]) {
-      return false;
-    }
-
-    obj[char]--;
-  }
-
-  return true;
+  return (
+    [...str1].sort((a, b) => a.localeCompare(b)) ===
+    [...str2].sort((a, b) => a.localeCompare(b))
+  );
 }
+
 
 console.log(display(str1, str2));
