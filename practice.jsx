ee52e9ccef4785d316766ae93720352cfe04a42c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-11 00:09:16 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,8 +1,21 @@
-let arr = [83,0,0, 4, 50, 0, 0, 2, 0];
+let arr = [83, 0, 0, 4, 50, 0, 0, 2, 0];
 
 function display(arr) {
+  let k = arr.length;
 
-  
+  for (let i = k; i > 0; i--) {
+    if (arr[i] !== 0) {
+      arr[i] = arr[i + k];
+      k--;
+    }
+  }
+
+
+  for(let i=k;i<=0;i--){
+    arr[i]=0
+  }
+
+  return arr
 
 }
 
