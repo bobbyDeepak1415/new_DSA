67ce88f7cd40fc17f20d781728ac5dd98732a26d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-12 22:22:32 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,22 +1,15 @@
 let arr = [83, 0, 0, 4, 50, 0, 0, 2, 0];
 
 function display(arr) {
-  let k = arr.length-1;
+  
+let k=0
 
-  for (let i = arr.length-1; i >= 0; i--) {
-    if (arr[i] !== 0) {
-      arr[k] = arr[i];
-      k--;
-    }
-  }
+for(let i=0;i<arr.length;i++){
+  let char=arr[i]
+  if(ar)
+}
 
-
-  for(let i=k;i>=0;i--){
-    arr[i]=0
-  }
-
-  return arr
 
 }
 
-console.log(display(arr));
+// console.log(display(arr));
