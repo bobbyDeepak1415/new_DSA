21291aaabd64408146f5da62b9c40d05297b6e54
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-19 23:36:52 +0530

    setting up

--- basics&arrays.js
+++ basics&arrays.js
@@ -257,11 +257,25 @@
 
 // -----------------------------------------------------------------------------------
 
-
 // 9.find all prime numbers from 1 to n
 
+function display(n) {
+  let arr = [];
 
+  for (let i = 2; i <= n; i++) {
+    let isPrime = true;
 
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
 
+    if (isPrime) arr.push(i);
+  }
 
+  return arr;
+}
 
+console.log(display(15));
