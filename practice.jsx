b5368493b9d3fd1936ccec70fb6054e358382d05
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-20 02:32:17 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,20 +1,20 @@
+let nums = [8, 45, -3, 5, -9, -2];
 
-let nums=[8,45,-3,5,-9,-2]
+function display(nums) {
+  let arr = [];
 
+  for (let i = 0; i < nums.length; i++) {
+let char=nums[i]
+    if(char<0){
+      arr.push(char)
+    }
 
+  }
 
-function display() {
-
-  let arr=[]
-
-
-  
-
-
+  return arr.reduce((a,b)=>a+b)
 
 }
 
-// console.log(display(nums));
-
+console.log(display(nums));
 
 // return the sum of all negative numbers,return all the negative numbers
