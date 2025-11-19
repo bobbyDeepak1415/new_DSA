c4e3a1ee9fde902f3b56d9fc4939d3c4567f8701
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-20 02:39:57 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,19 +1,24 @@
-let nums = [8, 45, -3, 5, -9, -2];
+// let nums = [8, 45, -3, 5, -9, -2];
 
-function display(nums) {
-  let arr = [];
+// function display(nums) {
+//   let arr = [];
 
 
-  for(let i=0;i<nums.length;i++){
-    if(nums[i]<0){
-      arr.push(nums[i])
-    }
-  }
+//   for(let i=0;i<nums.length;i++){
+//     if(nums[i]<0){
+//       arr.push(nums[i])
+//     }
+//   }
 
-  return arr.reduce((a,b)=>a+b)
+//   return arr.reduce((a,b))
 
-}
+// }
 
-console.log(display(nums));
+// console.log(display(nums));
+
+
+let arr=["app","pap","map","baap"]
+
+console.log(arr.sort((a,b)=>b.localeCompare(a)))
 
 // return the sum of all negative numbers,return all the negative numbers
