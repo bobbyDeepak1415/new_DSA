49fa89d407fa618ec35bab52d970872c54b397bc
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-19 23:52:53 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -4,7 +4,21 @@ function display(n) {
 
  let arr=[]
  
+ for(let i=2;i<=n;i++){
+
+  let isPrime=true
+  for(let j=2;i<j;j++){
+    if(i%j===0) {
+      isPrime=false
+      break
+    }
+  }
+
+  if(isPrime) arr.push(i)
+
+ }
  
+ return arr
 
 }
 
