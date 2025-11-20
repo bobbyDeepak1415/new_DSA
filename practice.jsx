2dfd1a31745020504f657a98b0ba7366fce16393
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-20 15:59:11 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -3,17 +3,8 @@
 let nums = [8, 45, -3, 5, -9, -2, -33, -6, 55];
 
 function display(nums) {
-  let negative = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 0) {
-      negative.push(nums[i]);
-    }
-  }
-
-  negative.sort();
-
-  return negative[0];
+  
 }
 
 console.log(display(nums));
