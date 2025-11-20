688418a85d01e9505fbee22eec1724823ac8f30f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-20 16:06:13 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,17 +1,18 @@
 // return biggest negative integer
 
-let nums = [8, 45, -3, 5, -9, -2, -33, -6, 55];
+let nums = [8, 45, -1, 5, -9, -2, -33, -6, 55];
 
 function display(nums) {
 
   let negative=[]
+  let positive=[]
   for(let i=0;i<nums.length;i++){
     if(nums[i]<0) negative.push(nums[i])
+    if(nums[i]>0) positive.push(nums[i])
   }
 
-  negative.sort()
 
-  return negative
+  return [...positive,...negative]
 
 }
 
