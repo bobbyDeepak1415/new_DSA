336f81f2c4ab9ecab3930d6fc2b166627a6f0ac0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-20 16:22:24 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -3,32 +3,19 @@
 let nums = [8, 45, -1, 5, -9, -2, -33, -6, 55];
 let arr=["cat","ball","zebra","eagle"]
 
-// function display(nums) {
+let employee={
+  name:"Bobby",
+  age:29,
+  active:true
 
-//   let negative=[]
-//   let positive=[]
-//   for(let i=0;i<nums.length;i++){
-//     if(nums[i]<0) negative.push(nums[i])
-//     if(nums[i]>0) positive.push(nums[i])
-//   }
+}
 
-//   positive=positive.sort((a,b)=>a-b)
-//   negative=negative.sort((a,b)=>a-b)
-
-//   return [...positive,...negative]
-
-// }
-
-// console.log(display(nums));
-
-
-// console.log(nums.sort((a,b)=>a-b))
 
 
 let arr1=[]
 
 for(let i=0;i<nums.length;i++){
-  arr1.push(Math.abs(nums[i]))
+  toString(nums[i])
 }
 
 console.log(arr1)
