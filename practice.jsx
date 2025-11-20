a5b3aab35ea8e90060a50ec266c1b44fd23459f0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-20 16:40:02 +0530

    setting up

--- practice.jsx
+++ practice.jsx
@@ -1,21 +1,14 @@
 // return biggest negative integer
 
-let nums = [8, 45, -1, 5, -9, -2, -33, -6, 55];
-let arr=["cat","ball","zebra","eagle"]
 
-let employee={
-  name:"Bobby",
-  age:29,
-  active:true
+let name="Bobby"
 
-}
+let employee = {
+  name: "Bobby",
+  age: 29,
+  active: true,
+};
 
-
-
-let arr1=[]
-
-for(let i=0;i<nums.length;i++){
-  toString(nums[i])
-}
-
-console.log(arr1)
+let nums=[45,33,34,35]
+console.log(nums.toLocaleString());
+console.log(name);
