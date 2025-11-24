d32d36889f50bf70f4475234f2d89902029341f5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 17:06:54 +0530

    first commit

--- practice.jsx
+++ practice.jsx
@@ -15,6 +15,35 @@ function display(str) {
 
     return count === 0;
   }
+
+  let result = [];
+  let queue = [str];
+  let found = false;
+  let visited = new Set([str]);
+
+  while (queue.length > 0) {
+    let current = queue.shift();
+
+    if (isValid(current)) {
+      result.push(current);
+
+      found = true;
+    }
+
+    if (found) continue;
+
+    for (let i = 0; i < current.length; i++) {
+      if (current[i] !== "(" && current[i] !== ")") continue;
+      let newStr = current.slice(0, 1) + current.slice(i + 1);
+
+      if (!visited.has(newStr)) {
+        visited.add(newStr);
+        queue.push(newStr);
+      }
+    }
+  }
+
+  return result;
 }
 
-// console.log(display(str))
+console.log(display(str));
