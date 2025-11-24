6f4804c6be5aea98612d3447d8a56a8aa74a4c63
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 17:25:29 +0530

    first commit

--- practice.jsx
+++ practice.jsx
@@ -7,10 +7,52 @@ function display(str) {
     function isValid(str){
         let count=0
 
-        for (let ch of str)
+        for (let ch of str){
+            if(ch==="(") count++
+            else if(ch===")"){
+                if(count===0) return false
+                count--
+            }
+        }
+
+        return count===0
 
     }
 
+
+    let result=[]
+
+    let found=false
+    let visited=new Set([str])
+    let queue=[str]
+
+
+
+    while(queue.length>0){
+        
+        let current=queue.shift()
+
+        if(isValid(current)){
+            result.push(current)
+            found=true
+
+        }
+
+        if(found=true) continue
+
+        for(let i=0;i<current.length;i++){
+            if(current[i]!=="(" && current[i]!==")") continue
+
+            let newStr = current.slice(0, i) + current.slice( i+1);
+
+            if()
+
+
+        }
+
+
+
+    }
 
 
 
