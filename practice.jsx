fc24531f4bdd0a552f1e74829bf888c06bb42668
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 17:46:41 +0530

    first commit

--- practice.jsx
+++ practice.jsx
@@ -1,15 +1,19 @@
-
 let str = "()())()";
 
+function display(str) {
+  function isValid(str) {
+    let count = 0;
 
-function display(str){
-
-    function isValid(str){
-let count=0
-
+    for (let ch of str) {
+      if (ch === "(") count++;
+      else if (ch === ")") {
+        if (count === 0) return false;
+        count--;
+      }
     }
 
-
+    return count === 0;
+  }
 }
 
 // console.log(display)
