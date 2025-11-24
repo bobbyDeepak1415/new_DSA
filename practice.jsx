3a4cca038c21f2dfa6b0b8349c457b41bda05917
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 17:27:54 +0530

    first commit

--- practice.jsx
+++ practice.jsx
@@ -3,60 +3,49 @@
 let str = "()())()";
 
 function display(str) {
+  function isValid(str) {
+    let count = 0;
 
-    function isValid(str){
-        let count=0
-
-        for (let ch of str){
-            if(ch==="(") count++
-            else if(ch===")"){
-                if(count===0) return false
-                count--
-            }
-        }
-
-        return count===0
-
+    for (let ch of str) {
+      if (ch === "(") count++;
+      else if (ch === ")") {
+        if (count === 0) return false;
+        count--;
+      }
     }
 
+    return count === 0;
+  }
 
-    let result=[]
+  let result = [];
 
-    let found=false
-    let visited=new Set([str])
-    let queue=[str]
+  let found = false;
+  let visited = new Set([str]);
+  let queue = [str];
 
+  while (queue.length > 0) {
+    let current = queue.shift();
 
-
-    while(queue.length>0){
-        
-        let current=queue.shift()
-
-        if(isValid(current)){
-            result.push(current)
-            found=true
-
-        }
-
-        if(found=true) continue
-
-        for(let i=0;i<current.length;i++){
-            if(current[i]!=="(" && current[i]!==")") continue
-
-            let newStr = current.slice(0, i) + current.slice( i+1);
-
-            if()
-
-
-        }
-
-
-
+    if (isValid(current)) {
+      result.push(current);
+      found = true;
     }
 
+    if (found==true) continue;
 
+    for (let i = 0; i < current.length; i++) {
+      if (current[i] !== "(" && current[i] !== ")") continue;
 
- 
+      let newStr = current.slice(0, i) + current.slice(i + 1);
+
+      if (!visited.has(newStr)) {
+        visited.add(newStr);
+        queue.push(newStr);
+      }
+    }
+  }
+
+  return result;
 }
 
-// console.log(display(str));
+console.log(display(str));
