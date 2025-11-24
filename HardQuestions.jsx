ef0634866d5fb7e508a97c540c2b46990bf211ea
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 13:59:01 +0530

    first commit

--- HardQuestions.jsx
+++ HardQuestions.jsx
@@ -3,20 +3,25 @@
 
 let str = "()())()";
 
-function diaplay(str){
+function display(str){
+
 
     function isValid(str){
         let count=0
+
         for(let ch of str){
             if(ch==="(") count++
             else if(ch===")"){
                 if(count===0) return false
                 count--
             }
-     
         }
+
         return count===0
+
     }
+
+
 
 }
 
