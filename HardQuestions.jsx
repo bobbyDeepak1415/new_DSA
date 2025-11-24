fb1c85f607f9fca9a7b9583b62ea836787533b9d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-25 02:44:07 +0530

    first commit

--- HardQuestions.jsx
+++ HardQuestions.jsx
@@ -1,111 +1,111 @@
 // 1.remove invalid paranthesis(301)
 
-// let str = "()())()";
+let str = "()())()";
 
-// function display(str) {
-//   function isValid(str) {
-//     let count = 0;
+function display(str) {
+  function isValid(str) {
+    let count = 0;
 
-//     for (let ch of str) {
-//       if (ch === "(") count++;
-//       else if (ch === ")") {
-//         if (count === 0) return false;
-//         count--;
-//       }
-//     }
+    for (let ch of str) {
+      if (ch === "(") count++;
+      else if (ch === ")") {
+        if (count === 0) return false;
+        count--;
+      }
+    }
 
-//     return count === 0;
-//   }
+    return count === 0;
+  }
 
-//   let result = [];
+  let result = [];
 
-//   let found = false;
-//   let queue = [str];
+  let found = false;
+  let queue = [str];
 
-//   let visited = new Set([str]);
+  let visited = new Set([str]);
 
-//   while (queue.length > 0) {
-//     let current = queue.shift();
+  while (queue.length > 0) {
+    let current = queue.shift();
 
-//     if (isValid(current)) {
-//       result.push(current);
+    if (isValid(current)) {
+      result.push(current);
 
-//       found = true;
-//     }
+      found = true;
+    }
 
-//     if (found) continue;
+    if (found) continue;
 
-//     for (let i = 0; i < current.length; i++) {
-//       if (current[i] !== "(" && current[i] !== ")") continue;
+    for (let i = 0; i < current.length; i++) {
+      if (current[i] !== "(" && current[i] !== ")") continue;
 
-//       let newStr = current.slice(0, i) + current.slice(i + 1);
+      let newStr = current.slice(0, i) + current.slice(i + 1);
 
-//       if (!visited.has(newStr)) {
-//         visited.add(newStr);
-//         queue.push(newStr);
-//       }
-//     }
-//   }
+      if (!visited.has(newStr)) {
+        visited.add(newStr);
+        queue.push(newStr);
+      }
+    }
+  }
 
-//   return result;
-// }
+  return result;
+}
 
-// console.log(display(str));
+console.log(display(str));
 
 // ----------------------------------------------------------------------------
 
 // 2.remove invalid paranthesis
 
-let words = ["This", "is", "an", "example", "of", "text", "justification."];
+// let words = ["This", "is", "an", "example", "of", "text", "justification."];
 
-function display(words, maxWidth) {
-  let i = 0;
-  let lines = [];
+// function display(words, maxWidth) {
+//   let i = 0;
+//   let lines = [];
 
-  while (i < words.length) {
-    let start = i;
-    let lineLength = words[i].length;
-    i++;
+//   while (i < words.length) {
+//     let start = i;
+//     let lineLength = words[i].length;
+//     i++;
 
-    while (i < words.length && lineLength + 1 + words[i].length <= maxWidth) {
-      lineLength += 1 + words[i].length;
-      i++;
-    }
+//     while (i < words.length && lineLength + 1 + words[i].length <= maxWidth) {
+//       lineLength += 1 + words[i].length;
+//       i++;
+//     }
 
-    // -----
-    let lineWords = words.slice(start, i);
+//     // -----
+//     let lineWords = words.slice(start, i);
 
-    // ------
-    if (i === words.length || lineWords.length === 1) {
-      let line = lineWords.join(" ");
-      line = line + " ".repeat(maxWidth - line.length);
-      lines.push(line);
-      continue;
-    }
+//     // ------
+//     if (i === words.length || lineWords.length === 1) {
+//       let line = lineWords.join(" ");
+//       line = line + " ".repeat(maxWidth - line.length);
+//       lines.push(line);
+//       continue;
+//     }
 
-    // ----
+//     // ----
 
-    let gaps = lineWords.length - 1;
-    let totalSpaces = maxWidth - (lineLength - gaps);
+//     let gaps = lineWords.length - 1;
+//     let totalSpaces = maxWidth - (lineLength - gaps);
 
-    let evenSpace = Math.floor(totalSpaces / gaps);
-    let extraSpace = totalSpaces % gaps;
+//     let evenSpace = Math.floor(totalSpaces / gaps);
+//     let extraSpace = totalSpaces % gaps;
 
-    // -------
+//     // -------
 
-    let justified = "";
+//     let justified = "";
 
-    for (let j = 0; j < lineWords.length - 1; j++) {
-      justified += lineWords[j];
-      justified += " ".repeat(evenSpace + (j < extraSpace ? 1 : 0));
-    }
+//     for (let j = 0; j < lineWords.length - 1; j++) {
+//       justified += lineWords[j];
+//       justified += " ".repeat(evenSpace + (j < extraSpace ? 1 : 0));
+//     }
 
-    justified += lineWords[lineWords.length - 1];
+//     justified += lineWords[lineWords.length - 1];
 
-    lines.push(justified);
-  }
+//     lines.push(justified);
+//   }
 
-  return lines;
-}
+//   return lines;
+// }
 
-console.log(display(words, 16));
+// console.log(display(words, 16));
--- practice.jsx
+++ practice.jsx
@@ -1,47 +1,11 @@
-let words = ["This", "is", "an", "example", "of", "text", "justification."];
 
-function display(words, maxWidth) {
-  let lines = [];
 
-  let i = 0;
 
-  while (i < words.length) {
-    let start = i;
-    let lineLength = words[i].length;
-    i++;
+let str = "()())()";
 
-    while (i < words.length && lineLength + 1 + words[i].length <= maxWidth) {
-      lineLength += 1 + words[i].length;
-      i++;
-    }
 
-    let lineWords = words.slice(start, i);
+function display(){
 
-    if (i === words.length || lineWords.length === 1) {
-      let line = lineWords.join(" ");
-      line = line + " ".repeat(maxWidth - line.length);
-      lines.push(line);
-      continue
-    }
-
-    let gaps = lineWords.length - 1;
-    let totalSpaces = maxWidth - (lineLength - gaps);
-    let evenSpace = Math.floor(totalSpaces / gaps);
-    let extraSpace = totalSpaces % gaps;
-
-    let justified = "";
-
-    for (let j = 0; j < lineWords.length-1; j++) {
-      justified += lineWords[j];
-      justified += " ".repeat(evenSpace + (j < extraSpace ? 1 : 0));
-    }
-
-    justified += lineWords[lineWords.length - 1];
-
-    lines.push(justified);
-  }
-
-  return lines;
 }
 
-console.log(display(words, 16));
+console.log(str)
