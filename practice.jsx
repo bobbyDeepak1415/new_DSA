aff23e4b01f041d984c85e48b3b23729be11713d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 19:54:47 +0530

    first commit

--- practice.jsx
+++ practice.jsx
@@ -1,19 +1,16 @@
 let words = ["This", "is", "an", "example", "of", "text", "justification."];
 
 function display(words, maxWidth) {
-  let i = 0;
-  let lines = [];
 
-  while (i < words.length) {
-    let start = i;
-    let lineLength = words[i].length;
-    i++;
 
-    while (i < words.length && lineLength + 1 + words[i].length <= maxWidth) {
-      lineLength += words[i].length;
-      i++;
+    let i=0
+    let lines=[]
+
+    while(i<words.length){
+        
     }
-  }
+
+  
 }
 
 // console.log(words,16)
