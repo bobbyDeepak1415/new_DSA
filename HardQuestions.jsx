08efeaba9c93b17b6478dfb397f48a6800733bfd
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 14:16:23 +0530

    first commit

--- HardQuestions.jsx
+++ HardQuestions.jsx
@@ -1,33 +1,28 @@
 // 1.remove invalid paranthesis(301)
 
-
 let str = "()())()";
 
-function display(str){
+function display(str) {
+  let result = [];
 
+  let found = false;
+  let queue = [str];
 
-    function isValid(str){
-        let count=0
+  let visited = new Set([str]);
 
-        for(let ch of str){
-            if(ch==="(") count++
-            else if(ch===")"){
-                if(count===0) return false
-                count--
-            }
-        }
+  function isValid(str) {
+    let count = 0;
 
-        return count===0
-
+    for (let ch of str) {
+      if (ch === "(") count++;
+      else if (ch === ")") {
+        if (count === 0) return false;
+        count--;
+      }
     }
 
-
-
+    return count === 0;
+  }
 }
 
-
 // console.log(display(str))
-
-
-
-
