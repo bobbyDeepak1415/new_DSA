adcf9a42c6218ab1b36863521e57e3a7d87e28e1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 17:35:39 +0530

    first commit

--- practice.jsx
+++ practice.jsx
@@ -1,51 +1,15 @@
-// return biggest negative integer
 
 let str = "()())()";
 
-function display(str) {
-  function isValid(str) {
-    let count = 0;
 
-    for (let ch of str) {
-      if (ch === "(") count++;
-      else if (ch === ")") {
-        if (count === 0) return false;
-        count--;
-      }
+function display(str){
+
+    function isValid(str){
+let count=0
+
     }
 
-    return count === 0;
-  }
 
-  let result = [];
-
-  let found = false;
-  let visited = new Set([str]);
-  let queue = [str];
-
-  while (queue.length > 0) {
-    let current = queue.shift();
-
-    if (isValid(current)) {
-      result.push(current);
-      found = true;
-    }
-
-    if (found==true) continue;
-
-    for (let i = 0; i < current.length; i++) {
-      if (current[i] !== "(" && current[i] !== ")") continue;
-
-      let newStr = current.slice(0, i) + current.slice(i + 1);
-
-      if (!visited.has(newStr)) {
-        visited.add(newStr);
-        queue.push(newStr);
-      }
-    }
-  }
-
-  return result;
 }
 
-console.log(display(str));
+// console.log(display)
