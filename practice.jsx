7bfb39b9494d83c530c389e620077c6e236d1ca4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-24 17:07:27 +0530

    first commit

--- practice.jsx
+++ practice.jsx
@@ -3,47 +3,7 @@
 let str = "()())()";
 
 function display(str) {
-  let count = 0;
-  function isValid(str) {
-    for (let ch of str) {
-      if (ch === "(") count++;
-      else if (ch === ")") {
-        if (count === 0) return false;
-        count--;
-      }
-    }
-
-    return count === 0;
-  }
-
-  let result = [];
-  let queue = [str];
-  let found = false;
-  let visited = new Set([str]);
-
-  while (queue.length > 0) {
-    let current = queue.shift();
-
-    if (isValid(current)) {
-      result.push(current);
-
-      found = true;
-    }
-
-    if (found) continue;
-
-    for (let i = 0; i < current.length; i++) {
-      if (current[i] !== "(" && current[i] !== ")") continue;
-      let newStr = current.slice(0, 1) + current.slice(i + 1);
-
-      if (!visited.has(newStr)) {
-        visited.add(newStr);
-        queue.push(newStr);
-      }
-    }
-  }
-
-  return result;
+ return str
 }
 
 console.log(display(str));
