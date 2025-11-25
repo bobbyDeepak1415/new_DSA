6acd470d9da25b3e18c29b0d8e1f3322820592d7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-26 02:54:01 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,38 +1,44 @@
 let words = ["What", "must", "be", "acknowledgment", "shall", "be"];
 function display(words, maxWidth) {
+  let lines = [];
 
-  let lines=[]
+  let i = 0;
 
-  let i=0
+  while (words.length > 0) {
+    let start = i;
+    let lineLength = words[i].length;
+    i++;
 
-  while(words.length>0){
-    let start=i
-    let lineLength=words[i].length
-    i++
-
-
-    while(words.length>0 && lineLength+1+words[i].length<=maxWidth){
-      lineLength+=1+words[i].length
-      i++
+    while (words.length > 0 && lineLength + 1 + words[i].length <= maxWidth) {
+      lineLength += 1 + words[i].length;
+      i++;
     }
 
-    let lineWords=words.slice(start,i)
+    let lineWords = words.slice(start, i);
 
-    // let gaps=lineWords.length-1
-    // let totalSpaces=
-    
-    if(i===words.length || lineWords.length===1){
+    if (i === words.length || lineWords.length === 1) {
+      let line = lineWords[i];
+      line = line + " ".repeat(maxWidth - lineWords.length);
+      lines.push(line);
+    }
+    let justified = "";
+    let gaps = lineWords.length - 1;
 
-      let line=lineWords[i]
-      line=line+" ".repeat(maxWidth-)
-    
+    let totalSpaces = maxWidth - (lineLength - gaps);
+    let evenSpaces = Math.floor(totalSpaces / gaps);
+    let extraSpaces = totalSpaces % gaps;
+
+    for (let j = 0; j < lineWords.length; j++) {
+      justified += lineWords[i];
+      justified += " ".repeat(evenSpaces - (j < extraSpaces ? 1 : 0));
+      continue;
     }
 
+    justified += lineWords[lineWords.length - 1];
+    lines.push(justified);
   }
 
-
-  return words
-
+  return words;
 }
 
 // console.log(display(words, 16));
