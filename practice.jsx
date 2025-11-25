7bff3074e386616495be980fe45cfe7fd875d598
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-26 02:39:14 +0530

    first commit

--- practice.jsx
+++ practice.jsx
@@ -1,8 +1,26 @@
 let words = ["What", "must", "be", "acknowledgment", "shall", "be"];
 function display(words, maxWidth) {
 
+  let lines=[]
+
+  let i=0
+
+  while(words.length>0){
+    let start=i
+    let lineLength=words[i].length
+    i++
+
+
+    while(words.length>0 && lineLength+1+words[i].length<=maxWidth){
+      lineLength+=words[i].length
+      i++
+    }
+
+  }
+
+
   return words
 
 }
 
-console.log(display(words, 16));
+// console.log(display(words, 16));
