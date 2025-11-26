a0679708270f3f1e86e7c994fdcb9a3366deb6a2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-26 23:18:36 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,8 +1,4 @@
-// function display() {}
+function display() {}
 
-// console.log(display());
+console.log(display());
 
-
-let arr1=[1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
-
-console.log(arr1.flat(2))
