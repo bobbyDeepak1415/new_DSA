05e1c87fcf3936c8edc3dedf2d765d2c58ede6f3
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-26 23:11:17 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -15,33 +15,32 @@ function display(words, maxWidth) {
     while (i < words.length && lineLength + 1 + words[i].length <= maxWidth) {
       lineLength += 1 + words[i].length;
       i++;
-
-      let lineWords = words.slice(start, i);
-
-      if ((i = words.length || lineWords.length === 1)) {
-        let line = lineWords.join(" ");
-        line = line + " ".repeat(maxWidth - line.length);
-        lines.push(line);
-        continue;
-      }
-
-      let justified = "";
-
-      let gaps = lineWords.length - 1;
-      let totalSpaces = maxWidth - (lineLength - gaps);
-      let evenSpaces = Math.floor(totalSpaces / gaps);
-      let extraSpaces = totalSpaces % gaps;
-
-      for (let j = 0; j < lineWords.length; j++) {
-        justified += lineWords[i];
-
-        justified += " ".repeat(evenSpaces + (j < extraSpaces ? 1 : 0));
-      }
-
-      justified += lineWords[lineWords.length - 1];
-
-      lines.push(justified);
     }
+    let lineWords = words.slice(start, i);
+
+    if ((i === words.length || lineWords.length === 1)) {
+      let line = lineWords.join(" ");
+      line = line + " ".repeat(maxWidth - line.length);
+      lines.push(line);
+      continue;
+    }
+
+    let justified = "";
+
+    let gaps = lineWords.length - 1;
+    let totalSpaces = maxWidth - (lineLength - gaps);
+    let evenSpaces = Math.floor(totalSpaces / gaps);
+    let extraSpaces = totalSpaces % gaps;
+
+    for (let j = 0; j < lineWords.length-1; j++) {
+      justified += lineWords[j];
+
+      justified += " ".repeat(evenSpaces + (j < extraSpaces ? 1 : 0));
+    }
+
+    justified += lineWords[lineWords.length - 1];
+
+    lines.push(justified);
   }
 
   return lines;
