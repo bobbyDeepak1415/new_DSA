9e2222bf41b74e8e2571a83e418a9d0743ed0003
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-26 22:52:21 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,7 +1,41 @@
-let str = "()())()";
+// let str = "()())()";
 
-function display(str) {
-   .
+let words = ["This", "is", "an", "example", "of", "text", "justification."]
+
+function display(words,maxWidth) {
+
+  let i=0
+
+  let lines=[]
+
+  while(i<words.length){
+    let start=i
+    let lineLength=words[i].length
+    i++
+
+    while(i<words.length &&  lineLength+1+words[i].length<=maxWidth){
+    lineLength+=1+words[i].length
+    i++
+
+    let lineWords=words.slice(start,i)
+
+    if(i=words.length || lineWords.length===1){
+      
+      let line=lineWords.join(" ")
+      line=line+" ".repeat(maxWidth-line.length)
+      lines.push(line)
+
+    }
+
+    let justified=""
+
+    
+
+
+    }
+
+  }
+   
 }
 
-// console.log(display(str));
+// console.log(display(words,16));
