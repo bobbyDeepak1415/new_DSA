3ada28e039d78af1e1ad5cd20db77fbfd6bd016e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-26 16:48:49 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,63 +1,51 @@
 let str = "()())()";
 
-
 function display(str) {
+  function isValid(str) {
+    let count = 0;
 
-  function isValid(str){
-
-    let count=0
-
-    for(let ch of str){
-      if(ch==="(") count++
-      else if(ch===")"){
-        if(count===0) return false
-        count--
+    for (let ch of str) {
+      if (ch === "(") count++;
+      else if (ch === ")") {
+        if (count === 0) return false;
+        count--;
       }
     }
 
-    return count===0
-
+    return count === 0;
   }
-
 
   let result = [];
 
-  let queue=[str]
+  let queue = [str];
 
-  let found=false
+  let found = false;
 
-  let checked=new Set([str])
+  let checked = new Set([str]);
 
+  while (queue.length > 0) {
+    let current = queue.shift();
 
-
-  while(queue.length>0 ){
-
-    let current=queue.shift()
-
-    if(isValid(current)){
-      result.push(current)
-      found===true
-      
+    if (isValid(current)) {
+      queue.push(current);
+      found === true;
     }
 
-    if(found) continue
+    if (found) continue;
 
+    for (let i = 0; i < current.length; i++) {
+      if (current[i] !== "(" || current[i] !== ")") continue;
 
-    for(let i=0;i<current.length;i++){
-      if(current[i]!=="(" || current[i]!==")") continue
+      let newStr = current.slice(0, i) + current.slice(i + 1);
 
-
-..
+      if (!checked.has(newStr)) {
+        checked.add(newStr);
+        result.push(newStr);
+      }
     }
-
-
-
   }
-
-
 
   return result;
 }
 
-// console.log(display(str));
-
+console.log(display(str));
