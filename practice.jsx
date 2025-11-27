a7416c2382a3e0b21a6f5e5cd7f4f48fbde81696
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-27 23:22:40 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,60 +1,51 @@
-
-
 let words = ["This", "is", "an", "example", "of", "text", "justification."];
 
+function display(words, maxWidth) {
+  let lines = [];
 
-function display(words,maxWidth) {
- 
+  let i = 0;
 
-    let lines=[]
+  while (words.length > 0) {
+    let start = i;
+    let lineLength = words[i].length;
+    i++;
 
-    let i=0
+    while (words.length > 0 && lineLength + 1 + words[i] <= maxWidth) {
+      lineLength += 1 + words[i].length;
+      i++;
+    }
+    let lineWords = words.slice(start, i);
 
-    while(words.length>0){
-        let start=i
-        let lineLength=words[i].length
-        i++
-        
-        
-        while(words.length>0 && lineLength+1+words[i]<=maxWidth){
-            lineLength+=1+words[i].length
-            i++
-            
-        }
-        let lineWords=words.slice(start,i)
+    if (i === words.length && lineWords.length === 1) {
+      let line = lineWords.join(" ");
 
-        if(i===words.length && lineWords.length===1){
+      line += lineLength[i];
+      line += " ".repeat(maxWidth - line.length);
+      lines.push(line);
 
-            
-            let line=lineWords.join(" ")
-
-            line+=lineLength[i]
-            line+=" ".repeat(maxWidth-line.length)
-            lines.push(line)
-
-            continue
-
-
-        }
-
-        let justified=""
-
-        let gaps=lineWords.length-1
-
-        let totalSpaces=maxWidth-(lineWords.length-1)
-
-let evenSpaces=Math.floor(totalSpaces/gaps)
-
-let extraSpaces=totalSpaces%gaps
-
-  for(let j=0;j<lineWords[i]-1;j++){
-    
-  }
-
+      continue;
     }
 
-  
+    let justified = "";
 
+    let gaps = lineWords.length - 1;
+
+    let totalSpaces = maxWidth - (lineWords.length - 1);
+
+    let evenSpaces = Math.floor(totalSpaces / gaps);
+
+    let extraSpaces = totalSpaces % gaps;
+
+    for (let j = 0; j < lineWords[j] - 1; j++) {
+      justified += lineWords[j];
+      justified += " ".repeat(evenSpaces + (j < extraSpaces ? 1 : 0));
+    }
+
+    justified += lineWords[lineWords.length - 1];
+    result.push(justified);
+  }
+
+  return result;
 }
 
 // console.log(display(words,16));
