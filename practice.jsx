1b9a8a6c233a90eb2f2c6c150cbe7b6c13476bdf
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-28 19:03:04 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,52 +1,12 @@
-let words = ["This", "is", "an", "example", "of", "text", "justification."];
+// let words = ["This", "is", "an", "example", "of", "text", "justification."];
 
-function display(words, maxWidth) {
-  let lines = [];
 
-  let i = 0;
+ let arr=[1, [2, [3]], 4];
 
-  while (i < words.length) {
-    let start = i;
+function display() {
 
-    let lineLength = words[i].length;
-    i++;
 
-    while (i < words.length && lineLength + 1 + words[i].length <= maxWidth) {
-      lineLength += 1 + words[i].length;
 
-      i++;
-    }
-
-    let lineWords = words.slice(start, i);
-
-    if (i === words.length || lineWords.length === 1) {
-      let line = lineWords.join(" ");
-      line = line + " ".repeat(maxWidth - line.length);
-      lines.push(line);
-
-      continue;
-    }
-
-    let justified = "";
-
-    let gaps = lineWords.length - 1;
-
-    let totalSpaces = maxWidth - (lineLength - gaps);
-    let evenSpaces = Math.floor(totalSpaces / gaps);
-    let extraSpaces = totalSpaces % gaps;
-
-    for (let j = 0; j < lineWords.length - 1; j++) {
-      justified += lineWords[j];
-      justified += " ".repeat(evenSpaces + (j < extraSpaces ? 1 : 0));
-    
-    }
-
-    justified += lineWords[lineWords.length - 1];
-
-    lines.push(justified);
-  }
-
-  return lines;
 }
 
-console.log(display(words, 16));
+// console.log(display(arr));
