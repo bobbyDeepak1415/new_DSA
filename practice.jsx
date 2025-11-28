b62fcba2811d2f4337be35fe251b94cad656a064
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-28 16:40:31 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -15,38 +15,36 @@ function display(words, maxWidth) {
       lineLength += 1 + words[i].length;
 
       i++;
-
     }
 
-      let lineWords = words.slice(start, i);
+    let lineWords = words.slice(start, i);
 
-      if (i === words.length || lineWords.length === 1) {
-        let line = lineWords.join(" ");
-        line = line + " ".repeat(maxWidth - line.length);
-        lines.push();
+    if (i === words.length || lineWords.length === 1) {
+      let line = lineWords.join(" ");
+      line = line + " ".repeat(maxWidth - line.length);
+      lines.push(line);
 
-        continue;
-      }
-
-      let justified = "";
-
-      let gaps = lineWords.length - 1;
-
-      let totalSpaces = maxWidth - (lineLength - gaps);
-      let evenSpaces = Math.floor(totalSpaces / gaps);
-      let extraSpaces = totalSpaces % gaps;
-
-      for (let j = 0; j < lineWords.length - 1; j++) {
-        justified += lineWords[j];
-        justified += " ".repeat(evenSpaces + (j < extraSpaces ? 1 : 0));
-        j++;
-      }
-
-      justified += lineWords[lineWords.length - 1];
-
-      lines.push(justified);
+      continue;
     }
-  
+
+    let justified = "";
+
+    let gaps = lineWords.length - 1;
+
+    let totalSpaces = maxWidth - (lineLength - gaps);
+    let evenSpaces = Math.floor(totalSpaces / gaps);
+    let extraSpaces = totalSpaces % gaps;
+
+    for (let j = 0; j < lineWords.length - 1; j++) {
+      justified += lineWords[j];
+      justified += " ".repeat(evenSpaces + (j < extraSpaces ? 1 : 0));
+    
+    }
+
+    justified += lineWords[lineWords.length - 1];
+
+    lines.push(justified);
+  }
 
   return lines;
 }
