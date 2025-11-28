91bd2413e65e9975e29a3684ec9e4a1afca02ca4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-28 16:24:20 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,37 +1,32 @@
-
-
 let words = ["This", "is", "an", "example", "of", "text", "justification."];
 
+function display(words, maxWidth) {
+  let lines = [];
 
-function display(words,maxWidth) {
-  
-    let lines=[]
+  let i = 0;
 
-    let i=0
+  while (i < words.length) {
+    let start = i;
 
-    while(i<words.length){
-        let start=i
+    let lineLength = words[i].length;
+    i++;
 
-        let lineLength=words[i].length
-        i++
+    while (i < words.length && lineLength + 1 + words[i].length <= maxWidth) {
+      lineLength += words[i].length;
 
-        while(i<words.length && lineLength+1+words[i].length<=maxWidth){
-            lineLength+=words[i].length
+      let lineWords = words.slice(start, i);
+      i++;
+
+      if (i === words.length || lineWords.length === 1) {
+        let line = lineWords.join(" ");
+        line = line + " ".repeat(maxWidth - line.length);
         
-            let lineWords=words.slice(start,i)
 
-            if(i===words.length || lineWords.length===1){
-                let line=lineWords.join()
-            }
-        }
-   
 
+
+      }
     }
-
-
-    
-
-
+  }
 }
 
 // console.log(display(words,16));
