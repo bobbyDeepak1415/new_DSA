11b30d96c225a3ec1a16517235d6611238d5eb5d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-28 19:09:19 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -5,18 +5,6 @@
 
 function display(arr) {
 
-let result=[]
-
-for(let i of arr){
-    if(Array.isArray(i)){
-        result.push(...display(i))
-    }else{
-        result.push(i)
-    }
-
-}
-
-return result
 
 }
 
