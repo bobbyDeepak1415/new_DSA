51caf4d5143c3b362ba23173772b82b3f0c4d4f7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-29 10:30:36 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -4,17 +4,6 @@ let arr = [1, [2, [[3], 66], 4]];
 
 function display(arr) {
 
-  let result=[]
-
-  for(let i of arr){
-    if(Array.isArray(i)){
-      result.push(...display(i))
-    }else{
-      result.push(i)
-    }
-  }
-
-  return result
 
 }
  
