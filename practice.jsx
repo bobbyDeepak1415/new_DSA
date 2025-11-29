f0acb53c7d2453913d8f9199b1bcc3d10c8d6cd8
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-29 13:08:27 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -3,8 +3,21 @@ let str = "AAABBCCCCCCCDDEEE";
 function display(str) {
   
 
+  let result=""
 
+  let count=1
+
+  for(let i=1;i<=str.length;i++){
+    if(str[i]===str[i-1]){
+      count++
+    }else if(str[i]!==str[i-1]){
+      result+=str[i-1]+count
+      count=1
+    }
+  }
+
+  return result
 
 }
 
-// console.log(display(str));
+console.log(display(str));
