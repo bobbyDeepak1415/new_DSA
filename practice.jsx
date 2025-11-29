dddf3d64a2b2964517fe2ab73e2688956b9376b1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-29 15:11:57 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,2 +1,17 @@
+let arr = [[3, 4], 90, [45, 67, [56, 33, 2, [40, 90], 89]]];
 
+function display(arr) {
+  let result = [];
 
+  for (let i of arr) {
+    if (Array.isArray(i)) {
+      result.push(...display(i));
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+console.log(display(arr));
