38bb149504eb95dae5f8c3f8e2f2cd56d0f87beb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-29 11:41:46 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -3,26 +3,21 @@
 let str = "AAABBCCCCCDD";
 
 function display(arr) {
+  let result = "";
 
-  let result=''
+  let count = 1;
 
-  let count=1
-
-  for(let i=1;i<str.length;i++){
-    if(str[i]===str[i-1]){
-      count++
-    }else{
-      result+=str[i-1]+count
-      count=1
+  for (let i = 1; i <= str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      count++;
+    } else {
+      result += str[i - 1] + count;
+      count = 1;
     }
   }
 
-  result+=str[str.length-1]+count
 
-  
-
-
-  
+  return result;
 }
 
-// console.log(display(str));
+console.log(display(str));
