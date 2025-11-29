06b7e5b4fb9b56232516143d83bcc2fef106eaf1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-29 17:29:03 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,9 +1,17 @@
 
-function display(str) {
+let str=""
 
+function display() {
+
+
+  let result=[]
+
+  for(let i=65;i<91;i++){
+    result.push(String.fromCharCode(i))
+  }
 
 
   return result;
 }
 
-// console.log(display());
+console.log(display());
