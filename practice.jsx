d3d2d3c41722fdf29f51baaf45165c17b73d3321
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-29 17:23:04 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,7 +1,20 @@
-let str="AABBBBBCCDDDDDDDDDD"
+let str="ABBBBBCCDDDDDDDDDD"
 
 function display(str) {
+
+
   let result = ""
+
+  let count=1
+
+  for(let i=1;i<=str.length;i++){
+    if(str[i]===str[i-1]){
+      count++
+    }else{
+      result+=str[i-1]+count
+      count=1
+    }
+  }
 
   return result;
 }
