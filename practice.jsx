4954cb3e5386d95b96bea428c7dbc5fdffc1498b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-29 17:47:01 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,7 +1,21 @@
-let str = "a      man  and     dog and one single   cat";
+let str = "the line has the largest word in it";
 
 function display(str) {
-  return str.trim().split(/\s+/)
+  // let result = "";
+
+  let words = str.split(" ")
+
+  let maxLength
+
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > result.length) {
+      result=words[i]
+    }
+    
+  }
+
+  return result
 }
 
 console.log(display(str));
