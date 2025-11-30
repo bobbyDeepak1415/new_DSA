d9bd15774049ea3eb92a2a82ee42a955534b1ae4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-11-30 10:06:51 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,13 +1,15 @@
 let str = "the line has the largest word in it";
 
 function display(str) {
-  // let result = "
-  
+  let result = "";
+
+  let arr = str.split(" ");
+
+  // for(let i=0;i<str.length;i++){
+  //   if()
+  // }
+
+  return arr;
 }
 
 console.log(display(str));
-
-
-
-
-// j
