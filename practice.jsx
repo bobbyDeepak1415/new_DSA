d1f259b7a9e78d70ef5bd18cbb623476d286723a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-02 02:25:57 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,15 +1,8 @@
-let arr = [[[45, 6], 6, [3, 4, [5, [6, 8]]]]];
+// let arr = [[[45, 6], 6, [3, 4, [5, [6, 8]]]]];
 
 function display(arr) {
-  let result = []
 
-  for(let i of arr){
-    if(Array.isArray(i)){
-      result.push(...display(i))
-    }else{
-      result.push(i)
-    }
-  }
+
 
 
   return result;
