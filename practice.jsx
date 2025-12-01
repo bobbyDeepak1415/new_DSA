dd980178a47defbfc195f4320f5f98159c0a1ebd
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-02 02:13:12 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -3,7 +3,19 @@ let str = "AAAABBCCCCDD";
 function display(str) {
  
 
-  
+  let result=""
+  let count=1
+
+  for(let i=1;i<=str.length;i++){
+    if(str[i]===str[i-1]){
+      count++
+    }else if (str[i] !== str[i - 1]) {
+      result+=result+str[i]
+      count=1
+    }
+  }
+
+  return result
 
 }
 
