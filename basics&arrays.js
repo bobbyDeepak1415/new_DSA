4d9254b7ceea71e8b200a8b73842fbf78c9bd89e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-03 02:01:22 +0530

    changes made

--- basics&arrays.js
+++ basics&arrays.js
@@ -320,3 +320,33 @@
 // console.log(display(arr));
 
 // -------------------------------------------------------------------------------------
+
+
+//11. combination sum (39)
+
+// function display(candidates, target) {
+//   let result = [];
+
+//   function backTracking(start, current, remaining) {
+//     if (remaining === 0) {
+//       result.push([...current]);
+//       return;
+//     }
+
+//     if (remaining < 0) {
+//       return;
+//     }
+
+//     for (let i = start; i < candidates.length; i++) {
+//       current.push(candidates[i]);
+//       backTracking(i, current, remaining - candidates[i]);
+//       current.pop();
+//     }
+//   }
+
+//   backTracking(0, [], target);
+
+//   return result;
+// }
+
+// console.log(display([2, 3, 6, 7], 7));
