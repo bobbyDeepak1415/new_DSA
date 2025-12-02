07b29bca8963608d2a756d70eb55a346316f02bf
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-02 23:25:47 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,16 +1,8 @@
 function display(candidates, target) {
   let result = [];
 
+return result
 
-
-  function backTrack(start,current,remaining){
-
-  }
-
-  backTrack(0)
-
-
-  return result;
 }
 
 // console.log(display([2, 3, 6, 7], 7));
