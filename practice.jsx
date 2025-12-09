729a85d8460eb2b91500b8811a44b55c3b51fee4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-10 00:19:04 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -3,17 +3,11 @@ let str = "PPPGGJJKKKKKK";
 function display(str) {
   let result = "";
 
-  let count = 0;
-
-  for (let i = 1; i <= str.length; i++) {
-    if (str[i] === str[i - 1]) {
-      count++;
+  for(let i=1;i<=str.length;i++){
+    if(str[i]===str[i-1]){
+      
     }
-    if (str[i] !== str[i - 1]) {
-      result += str[i - 1] + count;
-      count = 1;
-    }
-  }.
+  }
 
   return result;
 }
