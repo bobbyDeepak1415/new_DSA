2f901b0443e8b572da8cca2dff6a6a187c2887b9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-10 02:20:24 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -5,7 +5,20 @@ output = "P4G4J3 etc";
 function display(str) {
   let result = "";
 
-  
+  let obj = {};
+
+  for (let i of str) {
+    obj[i] = (obj[i] || 0) + 1;
+  }
+
+  let seen = new Set();
+
+  for (let i of str) {
+    if (!seen.has(i)) {
+      result += i + seen[i];
+      seen.add(i);
+    }
+  }
 
   return result;
 }
