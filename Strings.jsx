cd06a07ab0581919515d6bb936debc462d9aa50c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-10 18:21:09 +0530

    changes made

--- Strings.jsx
+++ Strings.jsx
@@ -313,5 +313,29 @@
 // ------------------------------------------------------------------
 
 
+// 11.count non consequtive pattern
 
+// let str = "PPPGGJJKGGKKPJ";
+
+// function display(str) {
+//   let result = "";
+
+//   let obj = {};
+
+//   for (let i of str) {
+//     obj[i] = (obj[i] || 0) + 1;
+//   }
+
+//   let seen = new Set();
+
+//   for (let i of str) {
+//     if (!seen.has(i)) {
+//       result += i + obj[i];
+//       seen.add(i);
+//     }
+//   }
+
+//   return result;
+// }
+// console.log(display(str));
 
--- practice.jsx
+++ practice.jsx
@@ -1,45 +1,12 @@
-// let str = "PPPGGJJKGGKKPJ";
 
-// function display(str) {
-//   let result = "";
-
-//   let obj = {};
-
-//   for (let i of str) {
-//     obj[i] = (obj[i] || 0) + 1;
-//   }
-
-//   let seen = new Set();
-
-//   for (let i of str) {
-//     if (!seen.has(i)) {
-//       result += i + obj[i];
-//       seen.add(i);
-//     }
-//   }
-
-//   return result;
-// }
-// // console.log(display(str));
 
 let str = "AAABBCCCCCDD";
 
 function display(str) {
   let result = "";
 
-  let count = 1;
-
-  for (let i = 1; i <= str.length; i++) {
-    let char = str[i];
-    if (char === str[i - 1]) {
-      count++;
-    } else {
-      result += str[i - 1] + count;
-      count = 1;
-    }
-  }
 
   return result;
 }
 
-console.log(display(str));
+// console.log(display(str));
