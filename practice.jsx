9e86084533c46ed601328e5034555dfe75716b55
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-11 01:34:59 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -8,7 +8,8 @@ function display(str) {
   for (let i = 1; i <= str.length; i++) {
     if (str[i] === str[i - 1]) {
       count++;
-    } else {
+    }
+    if (str[i] !== str[i - 1]) {
       result += str[i - 1] + count;
       count = 1;
     }
