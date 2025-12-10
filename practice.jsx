00b74359353f72e955214b167c628660afd461a9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-11 01:35:33 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,21 +1,11 @@
-let str = "AAABBCCCCCDD";
 
 function display(str) {
   let result = "";
 
-  let count = 1;
 
-  for (let i = 1; i <= str.length; i++) {
-    if (str[i] === str[i - 1]) {
-      count++;
-    }
-    if (str[i] !== str[i - 1]) {
-      result += str[i - 1] + count;
-      count = 1;
-    }
-  }
+  
 
   return result;
 }
 
-console.log(display(str));
+// console.log(display(str));
