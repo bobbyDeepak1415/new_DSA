bb75ade15efd646fe7c254dc303a1dc2f0235715
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-10 18:09:20 +0530

    changes made

--- practice.jsx
+++ practice.jsx
@@ -1,8 +1,17 @@
 let str = "PPPGGJJKGGKKPJ";
 
 function display(str) {
-  let result = ""
+  let result = "";
+
+  let obj = {};
+
+  for (let i of str) {
+    obj[i] = (obj[i] || 0) + 1;
+  }
+
+  return obj
+
+
+
 }
 // console.log(display(str));
-
-.
