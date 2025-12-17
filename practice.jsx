0207f741edb38e6a3b5cae60fb20136c208f0901
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-17 14:37:34 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,17 +1,8 @@
-let str = "the line has the largest wordsssssssss in it";
 
 function display(str) {
-  let arr1 = str.split(" ");
-
-  let result = arr1[0];
-
-  for (let i = 1; i < arr1.length; i++) {
-    if (arr1[i].length > arr1[i - 1].length) {
-      result = arr1[i];
-    }
-  }
+  
 
   return result;
 }
 
-console.log(display(str));
+// console.log(display(str));
