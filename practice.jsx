9f214445b251f94aefeab65ffb224ec784601d67
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-19 02:29:32 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,7 +1,19 @@
+let months = {
+  "Jan": 30,
+  "Feb": 29,
+  "Mar": 30,
+  "Apr": 31,
+  May: 30,
+  Jun: 30,
+  Jul: 31,
+  Aug: 30,
+  Sep: 30,
+  Oct: 31,
+  Nov: 30,
+  Dec: 31,
+};
 
 function display(str) {
-  
-
   return result;
 }
 
