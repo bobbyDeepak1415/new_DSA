8ad5a033c3b554995460bdb870eff2c079cac579
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-21 13:51:49 +0530

    setup done

--- basics&arrays.js
+++ basics&arrays.js
@@ -349,3 +349,44 @@
 // }
 
 // console.log(display([2, 3, 6, 7], 7));
+
+// ---------------------------------------------------------------------------
+
+// 12. find day of the date
+
+// let months = {
+//   Jan: 31,
+//   Feb: 28,
+//   Mar: 31,
+//   Apr: 30,
+//   May: 31,
+//   Jun: 30,
+//   Jul: 31,
+//   Aug: 31,
+//   Sep: 30,
+//   Oct: 31,
+//   Nov: 30,
+//   Dec: 31,
+// };
+
+// let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+// function display(month, date) {
+
+//   let totalDays=0
+
+//   for(let m in months){
+//     if(m===month) break
+//     totalDays+=months[m]
+//   }
+
+//   totalDays+=date-1
+//   let refIndex=3
+
+//   let dayIndex=(refIndex+totalDays)%7
+
+//   return days[dayIndex]
+
+// }
+
+// console.log(display("Mar", 12));
--- practice.jsx
+++ practice.jsx
@@ -1,17 +1,3 @@
-let months = {
-  Jan: 31,
-  Feb: 28,
-  Mar: 31,
-  Apr: 30,
-  May: 31,
-  Jun: 30,
-  Jul: 31,
-  Aug: 31,
-  Sep: 30,
-  Oct: 31,
-  Nov: 30,
-  Dec: 31,
-};
 
 // curDayIndex=(total+refDay)%7
 
@@ -19,29 +5,18 @@ let months = {
 // total+=date-1
 // monthPrev=
 
-let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-
-function display(month, date) {
-
-  let totalDays=0
-
-  for(let m in months){
-    if(m===month) break
-    totalDays+=months[m]
-  }
-
-  totalDays+=date-1
-  let refIndex=3
 
 
-  let dayIndex=(refIndex+totalDays)%7
+// find day of the date
 
-  return days[dayIndex]
 
+function display() {
+
+  
 
 }
 
-console.log(display("Mar", 12));
+// console.log(display());
 
 
 
