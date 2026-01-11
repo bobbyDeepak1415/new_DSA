16ba53b2c97aba58961a6822c2392c38dd459c6c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-11 20:42:46 +0530

    debugging

--- practice.jsx
+++ practice.jsx
@@ -1,35 +1,12 @@
 
 
 
-let str = "AAABBCCCCCDDBBBBB";
-let str1 = "PPPGGJJKGGKKPJ";
 
 
+function display() {
 
-function display(str) {
 
-
-    let obj={}
-
-    let result=""
-
-    for(let i of str){
-        obj[i]=(obj[i] || 0)+1
-
-    }
-
-    let checked=new Set()
-
-    for(let i=0;i<str.length;i++){
-        let char=str[i]
-        if(!checked.has(char)){
-            checked.add(char)
-            result+=char+obj[char]
-        }
-    }
-
-    return result
 
 }
 
-// console.log(display(str));
+// console.log(display());
