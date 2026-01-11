dfcaa4abafc4e4d83f06e316e78b673d9161e182
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-11 20:31:32 +0530

    debugging

--- practice.jsx
+++ practice.jsx
@@ -1,25 +1,10 @@
-let store = { "}": "{", "]": "[", ")": "(" };
 
-let str = "([[{{}]}]])";
-// let str = "{[[()]]}";
-
-// / let str = "AAABBCCCCCDD";
-// // let str1 = "PPPGGJJKGGKKPJ";
+let str = "PPPJJKRRSPPHHEEJ";
 
 function display(str) {
-  let stack = [];
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (["(", "{", "["].includes(char)) {
-      stack.push(char);
-    } else if ([")", "}", "]"].includes(char)) {
-      if (stack.pop() !== store[char]) return false;
-    }
-  }
+ 
 
-  return stack.length === 0;
+
 }
 
-console.log(display("[[[[]]]]"));
-// console.log(display("{{[}})"));
-// console.log(display("[{((),[],[()])}]"));
+// console.log(display());
