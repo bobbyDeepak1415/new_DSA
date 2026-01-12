fb75f6a19e1aa0d248f3b9bd58041901aafb1366
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-13 02:15:48 +0530

    set-up done

--- practice.jsx
+++ practice.jsx
@@ -1,6 +1,23 @@
 
 
 
+let months = {
+  Jan: 31,
+  Feb: 28,
+  Mar: 31,
+  Apr: 30,
+  May: 31,
+  Jun: 30,
+  Jul: 31,
+  Aug: 31,
+  Sep: 30,
+  Oct: 31,
+  Nov: 30,
+  Dec: 31,
+};
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
 
 
 function display() {
@@ -9,4 +26,4 @@ function display() {
 
 }
 
-// console.log(display());
+// console.log(display("Sep",28));
