c72f62b90a37a08a789c1061d07b91a0b74f5338
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-21 16:09:24 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,3 +1,15 @@
-function display() {}
 
-console.log(display());
+let str="AABBBCDDDEEEEE"
+function display(str) {
+    
+    let result=0
+
+    const checked=new Set()
+
+    for(let i=0;i<str.length;i++){
+        let item=str[i]
+if(checked[])
+    }
+}
+
+// console.log(display(str));
