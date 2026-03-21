b14bf0c158fc2547bd60b8b2fc6ef695c7812850
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-21 16:41:54 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -2,9 +2,19 @@ let str = "AABBBCDDDEEEEE";
 function display(str) {
   let obj = {};
 
-  
+  let result = "";
 
-  
+  for (let i of str) {
+    obj[i] = (obj[i] || 0) + 1;
+  }
+
+  let checked=new Set()
+
+  for(let i=0;i<str.length;i++){
+    let item=str[i]
+    if()
+  }
+
   return result;
 }
 
