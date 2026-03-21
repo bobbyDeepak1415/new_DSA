f30e2178c6c67e2c604a6c2955540a4b4ea8750f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-21 16:51:00 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -5,16 +5,10 @@ let arr = [
   ["E", 1],
 ];
 function display(arr) {
-  let sortedArr = arr.map((item) => item[0]);
 
-  return sortedArr.sort((a, b) => a.localeCompare(b));
+  return arr.sort((a, b) => a.localeCompare(b));
 }
 
 console.log(display(arr));
 
-arr = [
-  ["A", 2],
-  ["B", 4],
-  ["C", 3],
-  ["E", 1],
-];
+
