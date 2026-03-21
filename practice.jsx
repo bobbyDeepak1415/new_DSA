6bc564981a202adcc6e0c4c4cec7c099d09bbdd4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-21 16:44:47 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,24 +1,11 @@
-let str = "AABBBCDDDEEEEE";
+let arr = [
+  ["A", 2],
+  ["C", 3],
+  ["B", 4],
+  ["E", 1],
+];
 function display(str) {
-  let obj = {};
-
-  let result = "";
-
-  for (let i of str) {
-    obj[i] = (obj[i] || 0) + 1;
-  }
-
-  let checked = new Set();
-
-  for (let i = 0; i < str.length; i++) {
-    let item = str[i];
-    if (!checked.has(item)) {
-      checked.add(item);
-      result += item + obj[item];
-    }
-  }
-
-  return result;
+  
 }
 
-console.log(display(str));
+// console.log(display(arr));
