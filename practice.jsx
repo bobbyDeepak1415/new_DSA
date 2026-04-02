eebdce0b0632531ce37e0a9ce9e78cb3269be6df
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-02 16:00:08 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,2 +1,17 @@
-let str = "the line has the largest word in it";
+let arr1 = [3, 4, [4, [90, 89], [7, 9], 54, 78]];
 
+const display = (arr) => {
+  let result = [];
+
+  for (let i of arr) {
+    if (Array.isArray(i)) {
+      result.push(...display(i));
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
+
+console.log(display(arr1));
