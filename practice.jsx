4b5b22bbf2d2bcd6cac3af457942ecbfa6c8d902
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-03 00:24:24 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,14 +1,16 @@
 let arr1 = [3, 4, [4, [90, 89], [7, 9], 54, 78]];
 
 const display = (arr) => {
+  let result = [];
 
- let result=[]
+  for (let i of arr) {
+    if (Array.isArray(i)) {
+      result.push(...display(i));
+    } else {
+      result.push(i);
+    }
+  }
 
-
-
+  return result;
 };
 console.log(display(arr1));
-
-
-
-
