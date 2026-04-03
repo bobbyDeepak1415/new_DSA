2f181fe42841368e030e9e7e3899bb17f359a8fe
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-03 11:03:16 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,27 +1,28 @@
 let arr = [
   [1, 2, 3],
   [1, 1, 2, 3],
-  [1, 2, 2, 3, 3, 3, 1],
+  [1, 2, 2, 3, 3, 3],
 ];
 
 function display(arr) {
   let result = [];
 
   for (let i of arr) {
+    let count = 0;
     let obj = {};
 
-    let count = 0;
-    for (let item of i) {
-      obj[item] = (obj[item] || 0) + 1;
+    for (let val of i) {
+      obj[val] = (obj[val] || 0) + 1;
     }
 
-    for (let key in obj) {
-      if (obj[key] > 1) {
-        count++;
-      }
+    for(let key in obj){
+        if(obj[key]>1){
+            count++
+        }
     }
 
-    result.push(count);
+    result.push(count)
+
   }
 
   return result;
