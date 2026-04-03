aa966143df558ed8f2a0ca0832a4ea0afd49d93d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-03 10:56:35 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -7,24 +7,8 @@ let arr = [
 function display(arr) {
   let result = [];
 
+  for()
   
-for(let i of arr){
-    let obj={}
-    let count=0
-
-    for(let val of i){
-        obj[val]=(obj[val] || 0)+1
-    }
-
-    for(let key in obj){
-        if(obj[key]>1){
-            count++
-        }
-    }
-
-    result.push(count)
-}
-
   return result;
 }
 
