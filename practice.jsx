41d97f0e25a66465917739741f06db32072c524b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-11 17:23:47 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,32 +1,51 @@
-let arr = [
-  [1,1, 2,2,2,3,3, 3],
-  [1, 1, 2, 3],
-  [1, 2, 2, 3, 3, 3],
-];
+// let arr = [
+//   [1,1, 2,2,2,3,3, 3],
+//   [1, 1, 2, 3],
+//   [1, 2, 2, 3, 3, 3],
+// ];
 
-function display(arr) {
-  let result = [];
+// function display(arr) {
+//   let result = [];
 
-  for (let i of arr) {
-    let obj = {};
+//   for (let i of arr) {
+//     let obj = {};
 
-    let count = 0;
+//     let count = 0;
 
-    for (let val of i) {
-      obj[val] = (obj[val] || 0) + 1;
-    }
+//     for (let val of i) {
+//       obj[val] = (obj[val] || 0) + 1;
+//     }
 
-    for (let key in obj) {
-      if (obj[key] > 1) {
-        count++;
-      }
-    }
-    result.push(count);
-  }
+//     for (let key in obj) {
+//       if (obj[key] > 1) {
+//         count++;
+//       }
+//     }
+//     result.push(count);
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-console.log(display(arr));
+// console.log(display(arr));
 
 // ans:[0,1,2]
+
+
+
+
+let str1="(((}}}((]]"
+let str2="(([[]([])]))"
+
+
+function display(str){
+
+  let result=[]
+
+  for(let i of str){
+    
+  }
+
+}
+
+console.log(str2)
