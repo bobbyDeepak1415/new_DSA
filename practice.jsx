a4d8862b8fea459f4f56c01d938ac81282a6bc0d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-14 17:03:42 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -5,33 +5,9 @@
 // o/p: [{fruits: ["mango", "apple", "kiwi"],
 // vegetable: ["radish", "tomato", "potato"]}]
 
-
-let input=[{fruits: "mango", vegetable: "radish"},
-{fruits: "apple", vegetable: "tomato"},
-{fruits: "kiwi", vegetable: "potato"},]
-
-function display(givenObj){
-
-
-    
-let res = {}
-givenObj.forEach((val) => {
-  Object.entries(val).forEach(([key, val]) => {
-    if(!res[key]) {
-      res[key] = [val]
-    } else {
-      res[key].push(val)
-    }
-  })
-});
-
-return res
-
-}
-
-
-
-console.log(display(input))
-
-
+let input = [
+  { fruits: "mango", vegetable: "radish" },
+  { fruits: "apple", vegetable: "tomato" },
+  { fruits: "kiwi", vegetable: "potato" },
+];
 
