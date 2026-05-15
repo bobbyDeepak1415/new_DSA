7d6e70d54258d63a85e314240828210730f040a0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-16 02:52:20 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,49 +1,7 @@
-// i/p: [{fruits: "mango", vegetable: "radish"},
-// {fruits: "apple", vegetable: "tomato"},
-// {fruits: "kiwi", vegetable: "potato"},]
 
-// o/p: [{fruits: ["mango", "apple", "kiwi"],
-// vegetable: ["radish", "tomato", "potato"]}]
+// let input = [
+//   { fruits: "mango", vegetable: "radish" },
+//   { fruits: "apple", vegetable: "tomato" },
+//   { fruits: "kiwi", vegetable: "potato" },
+// ];
 
-let input = [
-  { fruits: "mango", vegetable: "radish" },
-  { fruits: "apple", vegetable: "tomato" },
-  { fruits: "kiwi", vegetable: "potato" },
-];
-
-// function display(givenObj) {
-//   let res = {};
-
-//   givenObj.forEach((obj) => {
-//     Object.entries(obj).forEach(([key, val]) => {
-//       if (!res[key]) {
-//         res[key] = [val];
-//       } else {
-//         res[key].push(val);
-//       }
-//     });
-//   });
-
-//   return [res];
-// }
-
-function display(givenObj) {
-
-  let res={}
-
-  givenObj.forEach((obj)=>{
-    Object.entries(obj).forEach(([key,val])=>{
-      if(!res[key]){
-        res[key]=val
-      }else{
-        res[key].push(val)
-      }
-    })
-  })
-
-return res
- 
-  
-}
-
-console.log(display(input));
