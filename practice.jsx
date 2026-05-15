0fcc2c312d72ea964b06319d1d2f605c2070fe45
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-16 03:02:09 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,7 +1,13 @@
-
 // let input = [
 //   { fruits: "mango", vegetable: "radish" },
 //   { fruits: "apple", vegetable: "tomato" },
 //   { fruits: "kiwi", vegetable: "potato" },
 // ];
 
+
+
+const shoppingCart = {
+  apple: 2,
+  bread: 3,
+  milk: 4,
+};
