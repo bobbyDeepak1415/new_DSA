f5d5c6dd47ae2c32efdd1294ae0e285f38980460
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-16 00:39:00 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -28,20 +28,8 @@ let input = [
 // }
 
 function display(givenObj) {
-  let res = {};
-
-  givenObj.forEach((obj) => {
-    Object.entries(obj).forEach(([key,val])=>{
-      if(!res[key]){
-        res[key]=val
-      }else{
-        res[key].push(val)
-      }
-    })
-   
-  });
-
-  return [res];
+ 
+  
 }
 
 console.log(display(input));
