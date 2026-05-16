502b12407871dc1fead5228aba0a989e6501da97
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-17 02:56:33 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,28 +1,26 @@
+let str = "AAABBCCDDAAA";
+
+function display(str) {
+  let result = "";
+
+  let obj = {};
+
+  for (let i of str) {
+    obj[i] = (obj[i] || 0) + 1;
+  }
+
+  let seen=new Set()
 
 
-
-let str="Hello World from Bobby"
-
-function display(str){
-
-    let result=[]
-
-
-    let arr=str.split(" ")
-
-
-    for(let i of arr){
-        let seen=new Set()
-        for(let j of i){
-            seen.add(j)
-        }
-
-        result.push(`${i}-${seen.size}`)
+  for(let i of str){
+    if(!seen.has(i)){
+        seen.add(i)
+        result+=i+obj[i]
     }
+  }
 
-    return result
+  return result
 
 }
 
-
-console.log(display(str))
+console.log(display(str));
