45815ddeac0cecc58193714cf99d52d98ab3d870
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-17 03:18:41 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,24 +1,23 @@
-
-
 let input = [
   { fruits: "mango", nuts: "almonds", vegetable: "radish" },
   { nuts: "brazil nuts", fruits: "apple", vegetable: "tomato" },
   { fruits: "kiwi", vegetable: "potato" },
 ];
 
-
 function display(givenObj) {
+  let res = {};
 
-    let res={}
-    
-
-    givenObj.forEach(obj => {
-        Object.entries(obj).forEach(([key,val])=>{
-            
-        })
+  givenObj.forEach((obj) => {
+    Object.entries(obj).forEach(([key, val]) => {
+      if (res[key]) {
+        res[key].push(val);
+      } else {
+        res[key] = [val];
+      }
     });
+  });
 
-    
+  return res;
 }
 
-// console.log(display(input));
+console.log(display(input));
