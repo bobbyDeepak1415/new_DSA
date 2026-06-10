aaf3a5d588743e98bf75de431883669bcdec4565
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-11 03:21:33 +0530

    setup done

--- Strings.jsx
+++ Strings.jsx
@@ -372,26 +372,22 @@
 
 // -----------------------------------------------------------------------------------------
 
-
 // 13.
 // let str="hello world from bobby"
 
-
 // function display(str){
-    
+
 //     let arr=str.split(" ")
 //     let result=[]
 
 //     for(let i of arr){
-    //         let capWord=i.charAt(0).toUpperCase()+i.slice(1)
-    //         let reverseWord=capWord.split("").reverse().join("")
-    //         result.push(reverseWord)
-    //     }
-    
-    //     return result.join(" ")
-    
-    // }
-    
-    // console.log(display(str))
-    
+//         let capWord=i.charAt(0).toUpperCase()+i.slice(1)
+//         let reverseWord=capWord.split("").reverse().join("")
+//         result.push(reverseWord)
+//     }
 
+//     return result.join(" ")
+
+// }
+
+// console.log(display(str))
