565386141564c5af5ed5c932fc91d8ceaac5514e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-11 03:31:18 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -2,8 +2,19 @@
 
 const nums = [2, 7, 11, 15];
 
-function display(){
+function display(arr,target){
 
+    let result=[]
+
+    for(let i of nums){
+        for(let j of nums){
+            if(i+j===target){
+                result.push(i,j)
+            }
+        }
+    }
+
+   return result
 }
 
-// console.log(display(nums))
+// console.log(display(nums,22))
