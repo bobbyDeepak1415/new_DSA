61801f5e4874b24bad634efc8bb10626b83c0cfb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-11 03:37:34 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,9 +1,26 @@
-const nums = [2, 7, 11, 15];
+// const nums = [2, 7, 11, 15];
 
-function display(arr, target) {
-  let result = [];
+let str="AAABBCCADDABDB"
+
+function display(str) {
+  let result = ""
+
+  let obj={}
+
+  for(let i of str){
+    obj[i]=(obj[i] || 0)+1
+  }
+
+  let seen=new Set()
+
+  for(let i of str){
+    if(!seen.has(i)){
+        seen.add(i)
+        result+=i+obj[i]
+    }
+  }
 
   return result;
 }
 
-console.log(display(nums, 9));
+console.log(display(str));
