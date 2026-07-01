1b5ee52129148b40bf7c38c3ec41bfb6f00372ff
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-01 15:37:07 +0530

    final changes done

--- practice.jsx
+++ practice.jsx
@@ -10,20 +10,19 @@ function display(str){
     
     let map={"}":"{",")":"(","]":'['}
 
-let result=[]
+    let stack=[]
 
-for(let i of str){
-    if(["[","{","("].includes(i)){
-result.push(i)
-    }else if(["]","}",")"].includes(i)){
-        if(result.pop()!==map[i]){
-return false
+    for(let i of str){
+        if(["{","[","("].includes(i)){
+            stack.push(i)
+        }else if(["}","]",")"].includes(i)){
+            if(stack.pop()!==map[i]){
+                return false
+            }
         }
     }
-}
 
-return result.length===0
-
+    return stack.length===0
 }
 
 
