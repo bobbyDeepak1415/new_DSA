159181ba95147d8607c2c9e82d50e65c12e7ea9d
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-01 15:37:32 +0530

    final changes done

--- practice.jsx
+++ practice.jsx
@@ -1,29 +1,2 @@
 
 
-let str="[[({{})]]"
-
-
-let str3="[[{{(())}}]]"
-
-
-function display(str){
-    
-    let map={"}":"{",")":"(","]":'['}
-
-    let stack=[]
-
-    for(let i of str){
-        if(["{","[","("].includes(i)){
-            stack.push(i)
-        }else if(["}","]",")"].includes(i)){
-            if(stack.pop()!==map[i]){
-                return false
-            }
-        }
-    }
-
-    return stack.length===0
-}
-
-
-console.log(display(str3))
