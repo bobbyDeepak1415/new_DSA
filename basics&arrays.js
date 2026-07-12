dd67cb2549b943d432e7fc0952fcab078e9cdd43
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-12 16:04:24 +0530

    setup done

--- basics&arrays.js
+++ basics&arrays.js
@@ -391,8 +391,18 @@
 
 // console.log(display("Mar", 12));
 
-
-
 // ----------------------------------------------------------------------------
 
+//13. Implement a function in javascript that paginates the list into pages of size N and returns the items for a given pageNumber
 
+// const subjects = ["Maths", "English", "Telugu", "Geography", "Physics"];
+
+// function display(subs, pageNo, pageSize) {
+//   let startIndex = (pageNo - 1) * pageSize;
+
+//   let lastIndex = startIndex + pageSize;
+
+//   return subs.slice(startIndex, lastIndex);
+// }
+
+// console.log(display(subjects, 3, 2));
--- practice.jsx
+++ practice.jsx
@@ -1,16 +1,14 @@
 const subjects = ["Maths", "English", "Telugu", "Geography", "Physics"];
 
 function display(subs, pageNo, pageSize) {
+  let startIndex = (pageNo - 1) * pageSize;
 
+  let lastIndex = startIndex + pageSize;
 
-    let startIndex=pageNo*pageSize-1
-
-    let lastIndex=startIndex+page
-
-    return subs.slice(startIndex,lastIndex)
-    
+  return subs.slice(startIndex, lastIndex);
 }
 
-console.log(display(subjects,3,2)); 
+console.log(display(subjects, 3, 2));
+
 
 // Implement a function in javascript that paginates the list into pages of size N and returns the items for a given pageNumber
