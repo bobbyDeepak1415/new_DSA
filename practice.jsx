6b02899238c9f3f3c654040a34f2d33770676003
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-12 15:59:39 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -2,13 +2,15 @@ const subjects = ["Maths", "English", "Telugu", "Geography", "Physics"];
 
 function display(subs, pageNo, pageSize) {
 
-    let startIndex=(pageNo-1)*pageSize
 
-    let endIndex=startIndex+pageSize
+    let startIndex=pageNo*pageSize-1
 
-    return subs.slice(startIndex,endIndex)
+    let lastIndex=startIndex+page
+
+    return subs.slice(startIndex,lastIndex)
+    
 }
 
-console.log(display(subjects,2,3));
+console.log(display(subjects,3,2)); 
 
 // Implement a function in javascript that paginates the list into pages of size N and returns the items for a given pageNumber
