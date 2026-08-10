59bde87fb16694183094130d0c2e3d4f3a3fa166
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-10 16:02:52 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,43 +1,21 @@
-// let nums = [1,0, 8, 0, 9, 11,12, 0, 13,0, 44];
+let nums = [1,0, 8, 0, 9, 11,12, 0, 13,0, 44];
 
-// function display(arr){
+function display(arr){
 
-//   let k=0
-
-//   for(let i=0;i<arr.length;i++){
-//     if(arr[i]!==0){
-//       let temp=arr[i]
-//       arr[i]=arr[k]
-//       arr[k]=temp
-//       k++
-//     }
-//   }
-
-//   return nums
-
-// }
-
-// console.log(display(nums))
-
-let arr = [1, 2, 2, 3, 4, 4, 5];
-
-function display(arr) {
-  let result = [];
-
-  let obj={}
-
-  for(let i of arr){
-    obj[i]=(obj[i] || 0)+1
-  }
+  let k=0
 
   for(let i=0;i<arr.length;i++){
-  let num=arr[i]
-if(obj[num]===1){
-  result.push(num)
-}
+    if(arr[i]!==0){
+      let temp=arr[i]
+      arr[i]=arr[k]
+      arr[k]=temp
+      k++
+    }
   }
 
-  return result;
+  return nums
+
 }
 
-console.log(display(arr));
+console.log(display(nums))
+
