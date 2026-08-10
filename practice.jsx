424cbd7dfcf2f8fef729abc7c5cb8a3dea33aa88
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-10 15:48:27 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,26 +1,17 @@
+let nums = [109,44,56,1,345, 8, 9, 11, 12, 13];
 
-
-
-
-let nums = [1, 8, 9, 11,12, 13, 44];
-
-function display(arr){
-  
-  for(let i=0;i<arr.length;i++){
-    for(let j=0;j<arr.length-1-i;j++){
-      if(arr[j]>arr[j+1]){
-let temp=arr[j+1]
-arr[j+1]=arr[j]
-arr[j]=temp
+function display(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
       }
     }
   }
 
-  return arr
-
+  return arr;
 }
 
-
-console.log(display(nums))
-
-
+console.log(display(nums));
