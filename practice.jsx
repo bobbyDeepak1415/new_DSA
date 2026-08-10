777b5bbfe7c1e0b407e79625e2d7d7b6d33247c0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-10 17:15:29 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,31 +1,3 @@
-// let temp=arr[i]=1
-// 
-// 
-// 
-
-let nums = [1,0, 8, 0, 9, 11,12, 0, 13,0, 44];
 
 
-
-function display(arr){
-
-  let k=0
-
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]!==0){
-      let temp=arr[i]
-      arr[i]=arr[k]
-      arr[k]=temp
-      k++
-
-    }
-  }
-
-
-
-  return nums
-
-}
-
-// console.log(display(nums))
 
