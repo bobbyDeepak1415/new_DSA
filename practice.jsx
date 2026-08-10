c14490e22f9a0fc8b631112fd7398e9aff8e3378
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-10 17:18:01 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,3 +1,26 @@
 
+let arr=[2,0,0,8,7,4]
 
 
+function display(arr){
+
+
+  let k=0
+
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]!==0){
+      let temp=arr[i]
+      arr[i]=arr[k]
+      arr[k]=temp
+      k++
+    }
+  }
+
+  return arr
+
+
+}
+
+
+
+console.log(display(arr))
