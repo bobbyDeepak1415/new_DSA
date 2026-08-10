68e0bb275daebf73e846be2021b228b082871bcb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-10 15:47:37 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,15 +1,26 @@
-let str = "suuccess";
 
-function display(str){
 
-  let obj={}
 
-  for(let i of str){
-    if(obj[i]) return i
-    obj[i]=true
+
+let nums = [1, 8, 9, 11,12, 13, 44];
+
+function display(arr){
+  
+  for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length-1-i;j++){
+      if(arr[j]>arr[j+1]){
+let temp=arr[j+1]
+arr[j+1]=arr[j]
+arr[j]=temp
+      }
+    }
   }
+
+  return arr
 
 }
 
 
-// console.log(display(str))
+console.log(display(nums))
+
+
