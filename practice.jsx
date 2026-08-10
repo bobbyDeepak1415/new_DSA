be000a702aa8b3da5dc73bcbc2e62e5173ef7463
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-10 17:23:06 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,17 +1,8 @@
-let arr = [2, 0, 0, 8, 7, 4];
 
-function display(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
 
-  return arr;
+
+function display(){
+
 }
 
-console.log(display(arr));
+// console.log(display())
