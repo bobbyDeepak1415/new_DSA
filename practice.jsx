0ed9a7e68fc36198c435f64edbf784434f49f52e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-24 23:46:40 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -12,6 +12,17 @@ function display(str){
 obj[i]=(obj[i] || 0)+1
     }
 
+    let seen=new Set()
+
+    for(let i of str){
+        if(!seen.has(i)){
+            seen.add(i)
+            result+=i+obj[i]
+        }
+    }
+
+    return result
+
 }
 
 // console.log(display(str))
