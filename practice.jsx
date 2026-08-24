fe3ca7f154f039c5d156546206818ec32ca4947e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-25 00:00:34 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,21 +1,25 @@
 let arr=[11,29,32,4,9,2,1]
 
 
+
 function display(arr){
 
     // let result=[]
 
 
-    // for(let i=0;i<arr.length;i++){
-    //     for(let j=0;j<arr.lenth-i-1;j++){
-    //         let temp=arr[j]
-    //          arr[j]=arr[j+1]
-    //          arr[j+1]=temp
-    //     }
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length-i-1;j++){
+            if(arr[j]<arr[j+1]){
 
-    // }
+                let temp=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
+            }
+        }
 
-return arr.sort((a,b)=>a-b)
+    }
+
+return arr
 
 }
 
