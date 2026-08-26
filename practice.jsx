4c2d0ee77905cf2d7fc37989cfc81d677f88e80e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-26 23:41:16 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,25 +1,12 @@
 
 
 
-let hayStack = "Bobby is a man and a Developer";
-let needle = "Developer";
-
-
-function display(hayStack,needle){
+function display(){
     
-    let arr1=hayStack.split("")
-    
-
-    
-for(let i=0;i<arr1.length;i++){
-    if(arr1[i]===needle){
-        return i
-    }
-}
 
 
 
 
 }
 
-console.log(display(hayStack,needle))
+// console.log(display())
