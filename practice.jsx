84cb523a71dd570102215880282900ebaf3defc3
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-17 22:58:33 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,30 +1,10 @@
 
-let str="[[{{(({[]}))}}]]"
-
-let str2="[{(})}]"
-
-let str3="[{}]"
 
 function display(str){
     
-let map={"]":"[","}":"{",")":"("}
-
-let stack=[]
-
-for(let i of str){
-    if(["{","[","("].includes(i)){
-stack.push(i)
-    }else if(["}","]",")"].includes(i)){
-        if(map[i]!==stack.pop(i)){
-            return false
-        }
-    }
-}
-
-return stack.length===0
 
 
 
 }
 
-// console.log(display(str2))
+// console.log(display())
