1c2d0c0ffbe4976520a0f7a3939ec7c7fc50561a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-22 02:05:28 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,10 +1,19 @@
 
-
-function display(str){
-    
-
-
-
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log("set",i); //1
+  }, 0);
+ 
+  Promise.resolve().then(() => {
+    console.log("P", i); //2
+  });
 }
+ 
+console.log("End"); //3
 
-// console.log(display())
+// p,0
+// p,1
+// p,2
+// set,o
+// set,1
+// set,2
