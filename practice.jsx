8b19b9d503b60cb773f6a536a96c2ffb96a8ce2e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-24 03:00:56 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -9,13 +9,13 @@ let arr = [2, 7, 11, 15];
 function display(arr,target){
 
 
-  for(let i=0;i<arr.length;i++){
-    for(let j=1;j<arr.length;j++){
-if(arr[i]+arr[j]===target){
-  return arr[i],arr[j]
-}
+for(let i=0;i<arr.length;i++){
+  for(let j=i+1;j<arr.length;j++){
+    if(arr[i]+arr[j]===target){
+      return [i,j]
     }
   }
+}
   
 
 
