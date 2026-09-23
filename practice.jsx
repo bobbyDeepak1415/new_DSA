9156aa206c731dc1b6303d8b0a112255db0d637c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-24 02:35:10 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -4,9 +4,23 @@
 let arr=[31,2,23,9,4,13]
 
 
+
 function display(arr){
 
-  return arr.reduce((a,b)=>a+b,0)
+  
+for(let i=0;i<arr.length;i++){
+  for(let j=0;j<arr.length-1-i;j++){
+    if(arr[j]>arr[j+1]){
+      let k=arr[j]
+
+      arr[j]=arr[j+1]
+      arr[j+1]=k
+    }
+  }
+}
+
+return arr
+
 
 }
 
