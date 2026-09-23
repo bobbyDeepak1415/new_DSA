b53c681b7732a4223f903a07d7b711e9b35ea3f4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-24 02:38:09 +0530

    setup done

--- basics&arrays.js
+++ basics&arrays.js
@@ -406,3 +406,34 @@
 // }
 
 // console.log(display(subjects, 3, 2));
+
+
+
+// -------------------------------------------------------------------
+
+
+// 14.sort an array without the sort method
+
+// let arr=[31,2,23,9,4,13]
+
+// function display(arr){
+
+  
+// for(let i=0;i<arr.length;i++){
+//   for(let j=0;j<arr.length-1-i;j++){
+//     if(arr[j]>arr[j+1]){
+//       let k=arr[j]
+
+//       arr[j]=arr[j+1]
+//       arr[j+1]=k
+//     }
+//   }
+// }
+
+// return arr
+
+
+// }
+
+
+// console.log(display(arr))
