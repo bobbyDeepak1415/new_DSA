9c7b4fa5ae6b833949fd182224091b261989572c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-24 13:22:59 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,3 +1,29 @@
 
+let arr = [2, 2, 2, 1, 1, 1, 2, 5, 5, 5, 5, 5, 5];
+
+
+
+function display(arr){
+
+    let obj={}
+let maxCount=0
+
+    for(let i of arr){
+        obj[i]=(obj[i] || 0)+1
+    }
+
+    for(let i of arr){
+        if(obj[i]>maxCount){
+            maxCount=obj[i]
+        }
+    }
+
+    return maxCount
+
+
+}
+
+console.log(display(arr))
+
 
 
