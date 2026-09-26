177110bfe94a66a522a779de2583d3b23ae6c4b3
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-26 19:15:15 +0530

    setup done

--- practice.jsx
+++ practice.jsx
@@ -1,17 +1,5 @@
-function display(n) {
-  let arr = [];
+function display(){
 
-  for(let i=0;i<=n;i++){
-    if(i%3===0&&i%5===0){
-        arr.push("FizzBuzz")
-    }else if(i%3===0){
-
-    }
-  }
-
-
-  return arr;
 }
 
-// console.log(display(13));
-
+console.log(display())
